bbd4851d3dc76eed5003151bcc62fcac4e83d789
Alvin Wang <alvin7@gmail.com>
2014-10-03 03:01:07 -0400

    added location handling for tooltips

--- js/tooltip.js
+++ js/tooltip.js
@@ -8,18 +8,39 @@
             // on mouseover
             origin = $(this);
             timeout = setTimeout(function(){
-                newTooltip = $('<div></div>');
-                newTooltip.text($(origin).attr('title'))
-                .addClass("material-tooltip")
+              newTooltip = $('<div></div>');
+              newTooltip.text($(origin).attr('title'))
+              .addClass("material-tooltip")
 
-                $('body').append(newTooltip);
-                // Set position of tooltip
-                var leftPos = $(origin).offset().left + $(origin).outerWidth()/2 - newTooltip.outerWidth()/2;
-                var topPos = $(origin).offset().top + $(origin).outerHeight();
-                newTooltip.css({top: topPos, left: leftPos});
-                // Animation
-                newTooltip.fadeIn(100);
-                origin.removeAttr('title')
+              $('body').append(newTooltip);
+              // Get cardinal position of tooltip
+              var location = origin.attr('data-tooltip');
+
+              // Set position of tooltip
+              var leftPos;
+              var topPos;
+              var margin = 15;
+              if (location == 'bottom') {              
+                leftPos = $(origin).offset().left + $(origin).outerWidth()/2 - newTooltip.outerWidth()/2;
+                topPos = $(origin).offset().top + margin;
+              }
+              else if (location == 'top') {
+                leftPos = $(origin).offset().left + $(origin).outerWidth()/2 - newTooltip.outerWidth()/2;
+                topPos = $(origin).offset().top - $(origin).outerHeight() - newTooltip.outerHeight() - margin;
+              }
+              else if (location == 'left') {
+                leftPos = $(origin).offset().left - newTooltip.outerWidth() - margin;
+                topPos = $(origin).offset().top - $(origin).outerHeight()/2 - newTooltip.outerHeight()/2;
+              }
+              else if (location == 'right') {
+                leftPos = $(origin).offset().left + $(origin).outerWidth() + margin;
+                topPos = $(origin).offset().top - $(origin).outerHeight()/2 - newTooltip.outerHeight()/2;
+              }
+
+              newTooltip.css({top: topPos, left: leftPos});
+              // Animation
+              newTooltip.fadeIn(100);
+              origin.removeAttr('title')
             },200);
         }, function(){
             clearTimeout(timeout);
