9f850e4d896a8ba6decc6d1a5eee708dd48af57d
Alvin Wang <alvin7@gmail.com>
2014-10-09 16:20:53 -0400

    changed dropdown to work on hover

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,42 +1,28 @@
 (function ($) {
   $.fn.dropdown = function () {
     var origin = $(this);
-    $("#"+ origin.attr('data-activates')).hide(0);
+    
+  
+    var activates = $("#"+ origin.attr('data-activates'));
+    var topMargin = 5;
+    activates.hide(0);
+    activates.css('width', origin.innerWidth());
+    activates.css('top', origin.offset().top + topMargin);
+    activates.css('left', origin.offset().left);
     
     // Click handler for list container
-    this.click(function(e){
+    origin.on('click', function(e){ // Mouse over
       e.stopPropagation();
-      var activates = $("#"+ origin.attr('data-activates'));
-      // If menu is present, either choose a selection or dismiss
-      if (activates.is(":visible")) {
-        console.log('here');
-      }
-      else {
-        activates.show(100);
-        
-        // Add onClick handlers to the list items
-        activates.on('click', '>', function(e) {
-          var item = $(this);
-          console.log(item.text());
-          origin.text(item.text());
-          // After click, close menu
-          activates.hide(100);
-          // Unbind handlers
-          activates.find("*").off('click');
-          
-        });
-        
-        // Add outside div handler to detect dismissal click
-        $(document).click(function (e) {
-          if (!activates.is(e.target) && activates.has(e.target).length === 0) {
-            activates.find("*").off();
-            activates.hide(100);
-            $(document).off(e);
-            
-          }
-        });
-      };
-      
+      activates.show(100);
     });
+    
+    // Doucment click handler        
+    $(document).click(function (e) {
+          if (!activates.is(e.target) && activates.has(e.target).length === 0) {
+            activates.hide(100);
+          }
+    });
+    
+    
   };
 }( jQuery ));
