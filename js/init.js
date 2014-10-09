64cc9025fe606c1c5e29193f593a95cdb15a2483
Chang Alan <alanchan@andrew.cmu.edu>
2014-10-09 16:13:21 -0400

    added SASS category

---
+++ js/init.js
@@ -0,0 +1,27 @@
+(function($){
+  $(function(){
+
+    // convert rgb to hex value string
+    function rgb2hex(rgb) {
+      if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+      rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+      function hex(x) {
+          return ("0" + parseInt(x).toString(16)).slice(-2);
+      }
+      return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+    }
+
+    $('.dynamic-color .col').each(function () {
+      $(this).children().each(function () {
+        var color = $(this).css('background-color');
+        var classes = $(this).attr('class');
+        $(this).html(rgb2hex(color) + " " + classes);
+        if (classes.indexOf("darken") >= 0) {
+          $(this).css('color', 'rgba(255,255,255,.9');
+        }
+      });
+    });    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
