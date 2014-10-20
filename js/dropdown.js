6d86e13fa63a21cb1cae075d686afb676183c3af
Alvin Wang <alvin7@gmail.com>
2014-10-20 15:56:54 -0400

    waves visual tweaks to lighten color

--- js/dropdown.js
+++ js/dropdown.js
@@ -10,19 +10,16 @@
 
     
     // Click handler for list container
-    origin.on('click', function(e){ // Mouse over
+    origin.on('mouseover', function(e){ // Mouse over
       activates.css('width', origin.innerWidth());
       activates.css('top', origin.offset().top + topMargin);
       activates.css('left', origin.offset().left);
-      e.stopPropagation();
-      activates.show(100);
+      activates.show(0, {queue: false});
     });
     
-    // Doucment click handler        
-    $(document).click(function (e) {
-          if (!activates.is(e.target) && activates.has(e.target).length === 0) {
-            activates.hide(100);
-          }
+//    // Doucment click handler        
+    origin.on('mouseout', function(e){ // Mouse out
+          activates.hide(50, {queue: false});
     });
     
     
