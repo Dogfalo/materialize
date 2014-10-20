bac0f2d47da4f5e7b046fc866e50acd18eeeb8bf
Alvin Wang <alvin7@gmail.com>
2014-10-20 17:03:21 -0400

    made dropdown styles more material

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,22 +4,27 @@
     
   
     var activates = $("#"+ origin.attr('data-activates'));
-    console.log(origin.offset());
-    var topMargin = 5;
+
     activates.hide(0);
 
     
     // Click handler for list container
     origin.on('mouseover', function(e){ // Mouse over
       activates.css('width', origin.innerWidth());
-      activates.css('top', origin.offset().top + topMargin);
+      activates.css('top', origin.offset().top);
       activates.css('left', origin.offset().left);
-      activates.show(0, {queue: false});
+      activates.show({duration: 200, easing: 'easeOutCubic'});
     });
     
-//    // Doucment click handler        
-    origin.on('mouseout', function(e){ // Mouse out
-          activates.hide(50, {queue: false});
+    // Doucment click handler        
+    activates.on('mouseleave', function(e){ // Mouse out
+          activates.hide({duration: 150, easing: 'easeOutCubic'});
+    });
+    
+    // Window Resize Reposition
+    $(window).on('resize', function(){
+      activates.css('top', origin.offset().top);
+      activates.css('left', origin.offset().left);
     });
     
     
