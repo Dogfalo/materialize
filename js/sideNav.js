dda9688b3c65868c3893c9f2be2ae737319dbf70
Alvin Wang <alvin7@gmail.com>
2014-11-07 15:47:58 -0500

    merged

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,4 +1,42 @@
 (function ($) {
+    // left: 37, up: 38, right: 39, down: 40,
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    var keys = [32, 33, 34, 35, 36, 37, 38, 39, 40];
+
+    function preventDefault(e) {
+      e = e || window.event;
+      if (e.preventDefault)
+          e.preventDefault();
+      e.returnValue = false;  
+    }
+
+    function keydown(e) {
+        for (var i = keys.length; i--;) {
+            if (e.keyCode === keys[i]) {
+                preventDefault(e);
+                return;
+            }
+        }
+    }
+
+    function wheel(e) {
+      preventDefault(e);
+    }
+
+    function disable_scroll() {
+      if (window.addEventListener) {
+          window.addEventListener('DOMMouseScroll', wheel, false);
+      }
+      window.onmousewheel = document.onmousewheel = wheel;
+      document.onkeydown = keydown;
+    }
+
+    function enable_scroll() {
+        if (window.removeEventListener) {
+            window.removeEventListener('DOMMouseScroll', wheel, false);
+        }
+        window.onmousewheel = document.onmousewheel = document.onkeydown = null;  
+    }
 
     $.fn.sideNav = function () {
       var $this = $(this);
@@ -10,6 +48,7 @@
             $(this).remove();
           } });
         menu_id.removeClass('active');
+        enable_scroll();
       }
 
       $this.click(function() {
@@ -17,6 +56,7 @@
           removeMenu();
         }
         else {
+          disable_scroll();
           menu_id.addClass('active');
 
           var overlay = $('<div id="sidenav-overlay"></div>');
