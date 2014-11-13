ee16b761d11fa7477bae2cca9d9525e022bbb6ad
Chang Alan <alanchan@andrew.cmu.edu>
2014-11-12 22:36:59 -0500

    parallax updates and demo

--- js/parallax.js
+++ js/parallax.js
@@ -3,31 +3,42 @@
     $.fn.parallax = function () {
       var window_width = $(window).width();
       // Parallax Scripts
-      return this.each(function() {
+      return this.each(function(i) {
         var $this = $(this);
         $this.addClass('parallax');
 
-        function updateParallax() {
+        function updateParallax(initial) {
           if (window_width > 480) {
-            var height = $this.height();
-            var bottom = $this.offset().top + height;
+            var container_height = $this.height();
+            var img_height = $this.children("img").height();
+            var parallax_dist = img_height - container_height;
+            var bottom = $this.offset().top + container_height;
             var top = $this.offset().top;
-            var windowHeight = $(window).height();
             var scrollTop = $(window).scrollTop();
-            var fromTop = scrollTop + top - (windowHeight / 2);
+            var windowHeight = $(window).height();
+            var windowBottom = scrollTop + windowHeight;
+            var percentScrolled = (windowBottom - top) / (container_height + windowHeight);
+            var parallax = -1 * parallax_dist * percentScrolled;
+            if (i === 0) {
+              console.log((windowBottom - top), (container_height + windowHeight), percentScrolled);
+            }
+      
 
-            if ((bottom > scrollTop) && (top < (scrollTop + windowHeight))) {   
-              var parallax = fromTop / 3;
-              
-              $this.children("img").first().css('top', parallax + "px");
+            if ((bottom > scrollTop) && (top < (scrollTop + windowHeight))) { 
+              if (initial) {
+                $this.children("img").first().stop().animate({bottom: parallax + "px"}, {duration: 300, queue: false, easing: 'easeInOutCubic'});
+              }
+              else {
+                $this.children("img").first().css('bottom', parallax + "px");
+              }
             }
 
           }
         }
-        updateParallax();
+        updateParallax(true);
         
         $(window).scroll(function() {
-          updateParallax();
+          updateParallax(false);
         });
 
       });
