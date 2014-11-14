64ca0d44ad6d0ffcef6a95b797de76b6ea5d08e5
Alvin Wang <alvin7@gmail.com>
2014-11-14 00:00:31 -0500

    modals are responsive

--- js/leanModal.js
+++ js/leanModal.js
@@ -5,54 +5,47 @@
         leanModal: function(options) {
  
             var defaults = {
-                top: 100,
-                overlay: 0.5
+                overlay: 0.6
             }
             
             var overlay = $("<div id='lean_overlay'></div>");
-            
             $("body").append(overlay);
-                 
             options =  $.extend(defaults, options);
- 
             return this.each(function() {
             
                 var o = options;
-               
                 $(this).click(function(e) {
-              
               	var modal_id = $(this).attr("href");
-
 				$("#lean_overlay").click(function() { 
                      close_modal(modal_id);                    
                 });
                 
                 $(modal_id).find('.modal_close').click(function(e) { 
                     e.preventDefault();
-                    close_modal(modal_id);
-                    // setTimeout( function(){ close_modal(modal_id); },200 );               
+                    close_modal(modal_id);            
                 });
                          	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
+              	
 
         		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
 
-        		$('#lean_overlay').fadeTo(200,o.overlay);
-
+        		$('#lean_overlay').fadeTo(200, o.overlay);
+                  
+                $(".modal").height($(".modal").height());
+                var modal_height = $(modal_id).outerHeight();
+        	  	var modal_width = $(modal_id).outerWidth();
+                  
         		$(modal_id).css({ 
         		
         			'display' : 'block',
         			'position' : 'fixed',
         			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px"
+                    'bottom' : '15%',
+        			'z-index': 1000
         		
         		});
 
-        		$(modal_id).animate({"top" : o.top + "px"
-                            , opacity: 1}, {duration: 300, easing: 'easeOutExpo'});
+        		$(modal_id).animate({opacity: 1}, {duration: 350, easing: 'easeOutQuart'});
 
                 e.preventDefault();
                 		
@@ -61,17 +54,12 @@
             });
 
 			function close_modal(modal_id){
-
         		$("#lean_overlay").fadeOut(200);
-
                 $(modal_id).fadeOut(200, function() {
-                    $(this).css('top', 0);
+//                    $(this).css('top', 0);
                 });
-                
-        		// $(modal_id).css({ 'display' : 'none' });
 			
 			}
-    
         }
     });
      
