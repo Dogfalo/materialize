5a8b72ccb6bb68b3acc2163edada3e40cfa92412
Chang Alan <alanchan@andrew.cmu.edu>
2014-11-20 11:25:00 -0500

    crude range working

--- js/forms.js
+++ js/forms.js
@@ -1,5 +1,7 @@
 (function ($) {
    
+
+  // Text based inputs
   var text_inputs = $('input[type=text], input[type=password], input[type=email]');  
 
   text_inputs.focus(function () {
@@ -7,10 +9,47 @@
   });
 
   text_inputs.blur(function () {
-    console.log($(this).val());
     if ($(this).val().length === 0) {
       $(this).siblings('label').removeClass('active');      
     }
   });
+
+
+  // Range Input
+  var range_input = $('input[type=range]');
+  var range_mousedown = false;
+
+  range_input.each(function () {
+    var thumb = $('<span class="thumb"><span class="value"></span></span>');
+    $(this).after(thumb);
+  });
+
+  range_input.on("mousedown", function() {
+    range_mousedown = true;
+  });
+  range_input.on("mouseup", function() {
+    range_mousedown = false;
+  });
+
+  range_input.on("mousemove", function(e) {
+    if (range_mousedown) {
+      var left = e.pageX - $(this).offset().left;
+      var width = $(this).outerWidth();
+
+      if (left < 0) {
+        left = 0;
+      }
+      else if (left > width) {
+        left = width;
+      }
+      console.log(left);
+      $(this).siblings('.thumb').addClass('active').css('left', left);
+      $(this).siblings('.thumb').find('.value').html($(this).val());      
+    }
+  });
+  range_input.on("mouseout", function() {
+    $(this).siblings('.thumb').removeClass('active');
+  });
+
 
 }( jQuery ));
