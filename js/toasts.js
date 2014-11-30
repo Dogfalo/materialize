a4251c8b5db8709a793eefd7ff81bf4d7816fcc5
Alvin Wang <alvin7@gmail.com>
2014-11-30 00:03:29 -0500

    added touch swipe away for toasts

--- js/toasts.js
+++ js/toasts.js
@@ -19,17 +19,16 @@ function toast(message, displayLength, className) {
                        {duration: 300,
                        easing: 'easeOutCubic',
                       queue: false});
-//        newToast.delay(displayLength)
-        newToast.velocity({"opacity": 0, marginTop: '-40px'}, { duration: 375,
-                                           easing: 'easeOutExpo',
-                                           queue: false, delay: displayLength,
-                                           complete: function(){$(this).remove()}
-                                          });
 
-//        .slideUp(250, function(){
-//            $(this).remove();
-//        });
-    
+    newToast.velocity({"opacity": 0, marginTop: '-40px'},
+                      { duration: 375,
+                        easing: 'easeOutExpo',
+                        queue: false, delay: displayLength + 99999,
+                        complete: function(){$(this).remove()}
+                      }
+                     );
+
+
     
     function createToast(message) {
         var toast = $('<div></div>');
@@ -38,6 +37,41 @@ function toast(message, displayLength, className) {
         var text = $('<span></span>');
         text.text(message);
         toast.append(text);
+        // Bind hammer
+        toast.hammer({prevent_default:false
+              }).bind('pan', function(e) {
+               
+                  var deltaX = e.gesture.deltaX;
+                  var activationDistance = 80;
+          
+          
+                  var opacityPercent = 1-Math.abs(deltaX / activationDistance);
+                if (opacityPercent < 0)
+                  opacityPercent = 0;
+          
+                  toast.velocity({left: deltaX, opacity: opacityPercent }, {duration: 50, queue: false, easing: 'easeOutQuad'});
+
+                }).bind('panend', function(e) {
+                  var deltaX = e.gesture.deltaX;
+                  var activationDistance = 80;
+          
+                  // If toast dragged past activation point
+                  if (Math.abs(deltaX) > activationDistance) {
+                    toast.velocity({marginTop: '-40px'},
+                                  { duration: 375,
+                        easing: 'easeOutExpo',
+                        queue: false,
+                        complete: function(){$(this).remove()}
+                      })
+                  } else {
+                    // Put toast back into original position
+                    toast.velocity({left: 0, opacity: 1},
+                                  { duration: 300,
+                        easing: 'easeOutExpo',
+                        queue: false
+                      })
+                  }
+                });
         return toast;
     }
 }
