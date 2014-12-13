0b977f08943031033b96e3b304925a98f65a7099
afifsohaili <mafif@booster.com>
2014-12-13 22:33:11 +0800

    Allow directly calling openModal on a modal element
    This will allow developers to call the modal on document ready

--- js/leanModal.js
+++ js/leanModal.js
@@ -1,29 +1,43 @@
 (function($) {
   $.fn.extend({
     openModal: function(options) {
-      var modal_id = $(this).attr("href");
+      debugger
+      var modal = this;
+      var overlay = $("<div id='lean_overlay'></div>");
+      $("body").append(overlay);
+
+      var defaults = {
+        opacity: 0.5,
+        in_duration: 300,
+        out_duration: 200,
+        ready: undefined,
+        complete: undefined
+      }
+
+      // Override defaults
+      options = $.extend(defaults, options);
+
       $("#lean_overlay").click(function() {
-        closeModal(modal_id);
+        closeModal(modal);
       });
 
-      $(modal_id).find(".modal_close").click(function(e) {
+      $(modal).find(".modal_close").click(function(e) {
         e.preventDefault();
-        closeModal(modal_id);
+        closeModal(modal);
       });
 
 
       $("#lean_overlay").css({ display : "block", opacity : 0 });
 
-      $(modal_id).css({
+      $(modal).css({
         display : "block",
         top: "4%",
         opacity: 0
       });
 
-
       $("#lean_overlay").velocity({opacity: options.opacity}, {duration: options.in_duration, queue: false, ease: "easeOutCubic"});
 
-      $(modal_id).velocity({top: "10%", opacity: 1}, {
+      $(modal).velocity({top: "10%", opacity: 1}, {
         duration: options.in_duration,
         queue: false,
         ease: "easeOutCubic",
@@ -38,7 +52,7 @@
       function closeModal(modal_id) {
         $("#lean_overlay").velocity( { opacity: 0}, {duration: options.out_duration, queue: false, ease: "easeOutQuart"});
         $(modal_id).fadeOut(options.out_duration, function() {
-          $(this).css({ top: 0});
+          $(modal_id).css({ top: 0});
           $("#lean_overlay").css({display:"none"});
 
           // Call complete callback
@@ -49,26 +63,14 @@
       }
     }
   });
+  
   $.fn.extend({
     leanModal: function(options) {
-      var defaults = {
-        opacity: 0.5,
-        in_duration: 300,
-        out_duration: 200,
-        ready: undefined,
-        complete: undefined
-      }
-
-      var overlay = $("<div id='lean_overlay'></div>");
-      $("body").append(overlay);
-
-      // Override defaults
-      options =  $.extend(defaults, options);
-
       return this.each(function() {
         // Close Handlers
         $(this).click(function(e) {
-          $(this).openModal(options);
+          var modal_id = $(this).attr("href");
+          $(modal_id).openModal(options);
           e.preventDefault();
         }); // done set on click
       }); // done return
