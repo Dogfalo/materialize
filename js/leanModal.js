3a838621c42b982e1614829e55da79f4fd660fc8
afifsohaili <mafif@booster.com>
2014-12-13 22:09:41 +0800

    Extract open modal behavior into a separate function

--- js/leanModal.js
+++ js/leanModal.js
@@ -1,5 +1,55 @@
 (function($) {
   $.fn.extend({
+    openModal: function(options) {
+      var modal_id = $(this).attr("href");
+      $("#lean_overlay").click(function() {
+        closeModal(modal_id);
+      });
+
+      $(modal_id).find(".modal_close").click(function(e) {
+        e.preventDefault();
+        closeModal(modal_id);
+      });
+
+
+      $("#lean_overlay").css({ display : "block", opacity : 0 });
+
+      $(modal_id).css({
+        display : "block",
+        top: "4%",
+        opacity: 0
+      });
+
+
+      $("#lean_overlay").velocity({opacity: options.opacity}, {duration: options.in_duration, queue: false, ease: "easeOutCubic"});
+
+      $(modal_id).velocity({top: "10%", opacity: 1}, {
+        duration: options.in_duration,
+        queue: false,
+        ease: "easeOutCubic",
+        // Handle modal ready callback
+        complete: function() {
+          if (typeof(options.ready) === "function") {
+            options.ready();
+          }
+        }
+      });
+
+      function closeModal(modal_id) {
+        $("#lean_overlay").velocity( { opacity: 0}, {duration: options.out_duration, queue: false, ease: "easeOutQuart"});
+        $(modal_id).fadeOut(options.out_duration, function() {
+          $(this).css({ top: 0});
+          $("#lean_overlay").css({display:"none"});
+
+          // Call complete callback
+          if (typeof(options.complete) === "function") {
+            options.complete();
+          }
+        });
+      }
+    }
+  });
+  $.fn.extend({
     leanModal: function(options) {
       var defaults = {
         opacity: 0.5,
@@ -18,56 +68,10 @@
       return this.each(function() {
         // Close Handlers
         $(this).click(function(e) {
-          var modal_id = $(this).attr("href");
-          $("#lean_overlay").click(function() {
-            close_modal(modal_id);
-          });
-
-          $(modal_id).find(".modal_close").click(function(e) {
-            e.preventDefault();
-            close_modal(modal_id);
-          });
-
-
-          $("#lean_overlay").css({ display : "block", opacity : 0 });
-
-          $(modal_id).css({
-            display : "block",
-            top: "4%",
-            opacity: 0
-          });
-
-
-          $("#lean_overlay").velocity({opacity: options.opacity}, {duration: options.in_duration, queue: false, ease: "easeOutCubic"});
-
-          $(modal_id).velocity({top: "10%", opacity: 1}, {
-            duration: options.in_duration,
-            queue: false,
-            ease: "easeOutCubic",
-            // Handle modal ready callback
-            complete: function() {
-              if (typeof(options.ready) === "function") {
-                options.ready();
-              }
-            }
-          });
-
+          $(this).openModal(options);
           e.preventDefault();
-        });
-      });
-
-      function close_modal(modal_id) {
-        $("#lean_overlay").velocity( { opacity: 0}, {duration: options.out_duration, queue: false, ease: "easeOutQuart"});
-        $(modal_id).fadeOut(options.out_duration, function() {
-          $(this).css({ top: 0});
-          $("#lean_overlay").css({display:"none"});
-
-          // Call complete callback
-          if (typeof(options.complete) === "function") {
-            options.complete();
-          }
-        });
-      }
+        }); // done set on click
+      }); // done return
     }
   });
 })(jQuery);
