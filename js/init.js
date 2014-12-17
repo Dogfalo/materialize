4075550e348c60aff6064e403f00d2a6269c08cb
Alvin Wang <alvin7@gmail.com>
2014-12-17 13:40:10 -0500

    container demo clarity enhanced

--- js/init.js
+++ js/init.js
@@ -142,6 +142,12 @@
       $('body .browser-window .container, .had-container').each(function(){
         $(this).toggleClass('had-container');
         $(this).toggleClass('container');
+        if ($(this).hasClass('container')) {
+          toggleContainersButton.text("Turn off Containers");
+        }
+        else {
+          toggleContainersButton.text("Turn on Containers");
+        }
       });
     });
 
