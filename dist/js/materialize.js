3941c0a0f4b0a4d5cd2f522d3d7c781ff876586c
Alvin Wang <alvin7@gmail.com>
2014-12-16 23:21:00 -0500

    tooltips now work properly with any size text and support multiple targets

--- dist/js/materialize.js
+++ dist/js/materialize.js
@@ -714,16 +714,20 @@ jQuery.extend( jQuery.easing,
     var newTooltip;
     var timeout;
     var counter;
-  var started;
-  var counterInterval
+    var started;
+    var counterInterval;
     $.fn.tooltip = function (options) {
+      var margin = 5;
+      started = false;
+
+      // Defaults
       var defaults = {
         delay: 350
       }
       options = $.extend(defaults, options);
       
-      var origin = $(this);
-      var margin = 5;
+      return this.each(function(){
+        var origin = $(this);
       
       // Create tooltip
       var newTooltip = $('<div></div');
@@ -732,60 +736,56 @@ jQuery.extend( jQuery.easing,
       
       var backdrop = $('<div></div').addClass('backdrop');
       backdrop.appendTo(newTooltip);
-      backdrop.css({ top: 0,
-                    left:0,
-              marginLeft: (newTooltip.outerWidth()/2) - (backdrop.width()/2) });
+      backdrop.css({ top: 0, left:0, marginLeft: (newTooltip.outerWidth()/2) - (backdrop.width()/2) });
       
-      started = false;
-      this.hover(function() {
+
+      // Mouse In
+      $(this).hover(function(e) {
+        e.stopPropagation();
         counter = 0;
-        
-       
         counterInterval = setInterval(function(){
           counter += 50;
           if (counter >= defaults.delay && started == false) {
             started = true
             newTooltip.css({ display: 'block' });
 
-            //    Bottom Position
+            // Bottom Position
             newTooltip.css({top: origin.offset().top + origin.outerHeight() + margin,
                   left: origin.offset().left + origin.outerWidth()/2 - newTooltip.outerWidth()/2 });
 
+            // Calculate Scale to fill
+            console.log(newTooltip.width());
+            scale_factor = newTooltip.width() / 11;
+            console.log(backdrop.width());
 
-            newTooltip.velocity({ opacity: 1, marginTop: '+10px'}, { duration: 250, queue: false });
+            newTooltip.velocity({ opacity: 1, marginTop: '+15px'}, { duration: 350, queue: false });
             backdrop.css({ display: 'block' })
-            .velocity({opacity:1},{duration: 200, delay: 0, queue: false})
-            .velocity({scale: 12}, {duration: 250, delay: 20, queue: false, easing: 'easeInOutQuad'});
+            .velocity({opacity:1},{duration: 75, delay: 0, queue: false})
+            .velocity({scale: scale_factor}, {duration: 300, delay: 0, queue: false, easing: 'easeInOutQuad'});
           }
-        }, 50);
-        
-        
-        
-//        Mouse Out
+        }, 50); // End Interval
+
+      // Mouse Out
       }, function(){
+        // Reset State
         clearInterval(counterInterval);
         counter = 0;
-        
-        
+
+        // Animate back
         newTooltip.velocity({
           opacity: 0, marginTop: '-10px'}, { duration: 225, queue: false, delay: 275 }
         );
-        
-        backdrop.velocity({opacity: 0, scale: 1},
-                          {duration:225,
-                           delay: 275, queue: false,
-                           complete: function(){
-                             backdrop.css('display', 'none');
-                             newTooltip.css('display', 'none');
-                             started = false;
-                           }
-                          }
-        );
-        
-        
-        
+        backdrop.velocity({opacity: 0, scale: 1}, {
+          duration:225,
+          delay: 275, queue: false,
+          complete: function(){
+            backdrop.css('display', 'none');
+            newTooltip.css('display', 'none');
+            started = false;}
+        });
       });
-    }
+    });
+  }
 }( jQuery ));;
 /*!
  * Waves v0.5.3
