74d25c5f485a61906711d3499eae0b037d39520d
Alvin Wang <alvin7@gmail.com>
2014-12-28 13:47:18 -0500

    basic positioning for all directions added

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,12 +1,11 @@
 (function ($) {
-    
-    var newTooltip;
     var timeout;
     var counter;
     var started;
     var counterInterval;
     $.fn.tooltip = function (options) {
       var margin = 5;
+      var newTooltip;
       started = false;
 
       // Defaults
@@ -39,27 +38,58 @@
             newTooltip.css({ display: 'block' });
 
             // Tooltip positioning
+            var originWidth = origin.outerWidth();
+            var originHeight = origin.outerHeight();
             var tooltipPosition =  origin.attr('data-position');
+            var tooltipHeight = newTooltip.outerHeight();
+            var tooltipWidth = newTooltip.outerWidth();
+            var tooltipVerticalMovement = '0px';
+            var tooltipHorizontalMovement = '0px';
+
             if (tooltipPosition === "top") {
             // Top Position
             newTooltip.css({
-              top: origin.offset().top - origin.outerHeight() - margin,
-              left: origin.offset().left + origin.outerWidth()/2 - newTooltip.outerWidth()/2});
+              top: origin.offset().top - tooltipHeight - margin,
+              left: origin.offset().left + originWidth/2 - tooltipWidth/2
+            });
+            tooltipVerticalMovement = '-10px';
             }
+            // Left Position
+            else if (tooltipPosition === "left") {
+              newTooltip.css({
+                top: origin.offset().top + originHeight/2 - tooltipHeight/2,
+                left: origin.offset().left - tooltipWidth - margin
+              });
+              tooltipHorizontalMovement = '-10px';
+            }
+            // Right Position
+            else if (tooltipPosition === "right") {
+              newTooltip.css({
+                top: origin.offset().top + originHeight/2 - tooltipHeight/2,
+                left: origin.offset().left + originWidth + margin
+              });
+              tooltipHorizontalMovement = '+10px';
+            }
+            else {
             // Bottom Position
-            newTooltip.css({top: origin.offset().top + origin.outerHeight() + margin,
-                  left: origin.offset().left + origin.outerWidth()/2 - newTooltip.outerWidth()/2 });
-
+            newTooltip.css({
+              top: origin.offset().top + origin.outerHeight() + margin,
+              left: origin.offset().left + originWidth/2 - tooltipWidth/2
+            });
+            tooltipVerticalMovement = '+10px';
+            }
 
             // Calculate Scale to fill
             scale_factor = newTooltip.width() / 8;
             if (scale_factor < 8)
               scale_factor = 8;
 
-            newTooltip.velocity({ opacity: 1, marginTop: '+10px'}, { duration: 350, queue: false });
+            
+            newTooltip.velocity({ opacity: 1, marginTop: tooltipVerticalMovement, marginLeft: tooltipHorizontalMovement}, { duration: 350, queue: false });
             backdrop.css({ display: 'block' })
             .velocity({opacity:1},{duration: 75, delay: 0, queue: false})
             .velocity({scale: scale_factor}, {duration: 300, delay: 0, queue: false, easing: 'easeInOutQuad'});
+
           }
         }, 50); // End Interval
 
@@ -71,7 +101,7 @@
 
         // Animate back
         newTooltip.velocity({
-          opacity: 0, marginTop: '-10px'}, { duration: 225, queue: false, delay: 275 }
+          opacity: 0, marginTop: 0, marginLeft: 0}, { duration: 225, queue: false, delay: 275 }
         );
         backdrop.velocity({opacity: 0, scale: 1}, {
           duration:225,
