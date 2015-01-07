17198e9f5d164961872dc8b0c222f84f032bff3a
Alvin Wang <alvin7@gmail.com>
2015-01-07 13:13:25 -0500

    table of contents should rarely overlap with footer now

--- js/init.js
+++ js/init.js
@@ -31,7 +31,8 @@
       var toc_offset = $('.table-of-contents').first().offset().top;
       $('.table-of-contents').each(function() {
         var origin = $(this);
-        origin.pushpin({ top: toc_offset, bottom: $(document).height() - $('footer').height(), offset: $('.tabs-wrapper').height() });
+        origin.pushpin({ top: toc_offset,
+          bottom: $(document).height() - window.innerHeight });
       });
     }
 
