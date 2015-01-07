bc7498cb063ad5ea3cd7a564e1da67846d22f9cd
Alvin Wang <alvin7@gmail.com>
2015-01-07 16:05:05 -0500

    Navbar scrollbar enabled on mobile through touch detection

--- js/init.js
+++ js/init.js
@@ -122,6 +122,19 @@
       });
     });
 
+    // Detect touch screen and enable scrollbar if necessary
+    function is_touch_device() {
+      try {
+        document.createEvent("TouchEvent");
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+    if (is_touch_device()) {
+      $('#nav-mobile').css({ overflow: 'auto'})
+    }
+
 
     // Plugin initialization
     $('.slider').slider({full_width: true});
