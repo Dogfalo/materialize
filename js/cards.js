bc7d98f8e3486c45ac3462ee0f2cedf5c9eb24e1
Chang Alan <alanchan@andrew.cmu.edu>
2015-01-17 16:39:41 -0500

    small cards bugfix

--- js/cards.js
+++ js/cards.js
@@ -4,17 +4,16 @@
     $(document).on('click.card', '.card', function (e) {
       if ($(this).find('.card-reveal').length) {
         if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
-          $(this).find('.card-reveal').velocity({translateY: 0}, {duration: 300, queue: false, easing: 'easeOutQuad'});        
+          $(this).find('.card-reveal').velocity({translateY: 0}, {duration: 300, queue: false, easing: 'easeOutQuad'});
         }
-        else if ($(e.target).is($('.card .card-title')) || 
-                 $(e.target).is($('.card .card-title i')) ||
-                 $(e.target).is($('.card .card-image')) ) {
-          $(this).find('.card-reveal').velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeOutQuad'});        
+        else if ($(e.target).is($('.card .activator')) ||
+                 $(e.target).is($('.card .activator i')) ) {
+          $(this).find('.card-reveal').velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeOutQuad'});
         }
       }
 
 
-    });  
+    });
 
   });
 }( jQuery ));
