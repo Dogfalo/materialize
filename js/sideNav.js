a409efa5e12d054948a4554171d95ea598380317
Chang Alan <alanchan@andrew.cmu.edu>
2015-01-21 16:53:31 -0500

    sideNav methods

--- js/sideNav.js
+++ js/sideNav.js
@@ -41,7 +41,8 @@
 
     // }
 
-    $.fn.sideNav = function (options) {
+  var methods = {
+    init : function(options) {
       var defaults = {
         activationWidth: 70,
         edge: 'left'
@@ -246,7 +247,27 @@
 
             return false;
           });
-});
+      });
 
-};
+
+    },
+    show : function() {
+      this.trigger('click');
+    },
+    hide : function() {
+      $('#sidenav-overlay').trigger('click');
+    }
+  };
+
+
+    $.fn.sideNav = function(methodOrOptions) {
+      if ( methods[methodOrOptions] ) {
+        return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+      } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+        // Default to "init"
+        return methods.init.apply( this, arguments );
+      } else {
+        $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.tooltip' );
+      }
+    }; // PLugin end
 }( jQuery ));
