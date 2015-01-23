811135757ed0dbbdeb87a141246d15e04a511a2a
Chang Alan <alanchan@andrew.cmu.edu>
2015-01-23 13:06:54 -0500

    textarea resizes with preset value

--- js/forms.js
+++ js/forms.js
@@ -68,9 +68,17 @@
         content = null;
         $('body').append(hiddenDiv);
     }
-    var hiddendiv = $('.hiddendiv');
     var text_area_selector = '.materialize-textarea';
     $('.hiddendiv').css('width', $(text_area_selector).width());
+
+    $(text_area_selector).each(function () {
+      if ($(this).val().length) {
+        content = $(this).val();
+        content = content.replace(/\n/g, '<br>');
+        hiddenDiv.html(content + '<br>');
+        $(this).css('height', hiddenDiv.height());
+      }
+    });
       $('body').on('keyup keydown',text_area_selector , function () {
         // console.log($(this).val());
         content = $(this).val();
