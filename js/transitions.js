abce047f07ef16fe4803ed7939da7de0dfbcff93
Chang Alan <alanchan@andrew.cmu.edu>
2015-02-09 14:26:04 -0500

    started scrollfire plugin

--- js/transitions.js
+++ js/transitions.js
@@ -3,17 +3,32 @@
 
     var time = 0;
 
-    $('ul.staggered-list li').velocity(
+    // Horizontal staggered list
+    $('ul.staggered-list.horizontal li').velocity(
         { translateX: "-100px"},
         { duration: 0 });
 
-    $('ul.staggered-list li').each(function() {
+    $('ul.staggered-list.horizontal li').each(function() {
       $(this).velocity(
         { opacity: "1", translateX: "0"},
         { duration: 800, delay: time, easing: [60, 10] });
       time += 120;
     });
 
+    time = 0
+    // Vertical Staggered list
+    $('ul.staggered-list.vertical li').velocity(
+        { translateY: "100px"},
+        { duration: 0 });
+
+    $('ul.staggered-list.vertical li').each(function() {
+      $(this).velocity(
+        { opacity: "1", translateY: "0"},
+        { duration: 800, delay: time, easing: [60, 25] });
+      time += 120;
+    });
+
+    // Fade in and Scale
     $('.fade-in.scale').velocity(
         { scaleX: .4, scaleY: .4, translateX: -600},
         { duration: 0});
