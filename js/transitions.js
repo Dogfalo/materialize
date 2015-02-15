fd09855f2075a5516b9c862a7576987e9b5ae57a
Adrian Mester <ilogik@users.noreply.github.com>
2015-02-15 04:03:25 +0200

    removed console.log

--- js/transitions.js
+++ js/transitions.js
@@ -29,7 +29,6 @@
          offset: 200,
          callback: 'showStaggeredList("ul.staggered-list.' + label + '")'});
     });
-    console.log(staggeredListOptions);
     scrollFire(staggeredListOptions);
 
 
