91d4c19691b41584438781f6ee58e9ab52047294
Chang Alan <alanchan@andrew.cmu.edu>
2015-02-23 15:38:46 -0500

    revert overflow changes to cards

--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,6 @@
 (function ($) {
   $(document).ready(function() {
 
-    $('.card > .card-reveal').each(function() {
-      $(this).parent().css('overflow', 'hidden');
-    });
-
     $(document).on('click.card', '.card', function (e) {
       if ($(this).find('.card-reveal').length) {
         if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
