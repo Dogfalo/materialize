1f66e91f8ef7d1964a4ad2f6c38fd72c252d271d
Chang Alan <alanchan@andrew.cmu.edu>
2015-02-27 12:27:27 -0500

    fixed table-of-contents issue on about and getting-started

--- js/init.js
+++ js/init.js
@@ -30,6 +30,9 @@
     if ($('nav').length) {
       $('.toc-wrapper').pushpin({ top: $('nav').height() });
     }
+    else if ($('#index-banner').length) {
+      $('.toc-wrapper').pushpin({ top: $('#index-banner').height() });
+    }
     else {
       $('.toc-wrapper').pushpin({ top: 0 });
     }
