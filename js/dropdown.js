02d8710ad28753c4404fd83e73c9982197ce46e9
Chang Alan <alanchan@andrew.cmu.edu>
2015-03-03 18:07:04 -0500

    automatic switch to right align near edge of page

--- js/dropdown.js
+++ js/dropdown.js
@@ -13,7 +13,6 @@
       outDuration: 225,
       constrain_width: true, // Constrains width of dropdown to the activator
       hover: true,
-      alignment: 'left',
       gutter: 0, // Spacing from edge
       belowOrigin: false
     }
@@ -34,8 +33,6 @@
         options.constrain_width = origin.data('constrainwidth');
       if (origin.data('hover') != undefined)
         options.hover = origin.data('hover');
-      if (origin.data('alignment') != undefined)
-        options.alignment = origin.data('alignment');
       if (origin.data('gutter') != undefined)
         options.gutter = origin.data('gutter');
       if (origin.data('beloworigin') != undefined)
@@ -67,10 +64,14 @@
       if (options.belowOrigin == true) {
         offset = origin.height();
       }
+
       // Handle edge alignment
+      var offsetLeft = origin.offset().left;
+
       var width_difference = 0;
       var gutter_spacing = options.gutter;
-      if (options.alignment == 'right') {
+
+      if (offsetLeft + activates.innerWidth() > $(window).width()) {
         width_difference = origin.innerWidth() - activates.innerWidth();
         gutter_spacing = gutter_spacing * -1;
       }
