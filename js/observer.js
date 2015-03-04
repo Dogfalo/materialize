f310d01235ec6a7a6bab1f1231539c88d1775c1d
Alvin Wang <alvin7@gmail.com>
2015-03-03 19:06:22 -0500

    Added mutation observer

---
+++ js/observer.js
@@ -0,0 +1,25 @@
+var observeDOM = (function() {
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+  eventListenerSupported = window.addEventListener;
+
+  return function(obj, callback) 
+    if ( MutationObserver ) {
+      // define a new observer
+      var obs = new MutationObserver(function(mutations, observer){
+        if ( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
+          callback(mutations);
+      });
+      // have the observer observe foo for changes in children
+      obs.observe( obj, { childList:true, subtree:true });
+    }
+    else if ( eventListenerSupported ) {
+      obj.addEventListener('DOMNodeInserted', callback, false);
+      obj.addEventListener('DOMNodeRemoved', callback, false);
+    }
+     }
+    })();
+
+// Observe a specific DOM element:
+observeDOM( document.body ,function(mutations){ 
+  console.log(mutations);
+});
