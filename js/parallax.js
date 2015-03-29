3f3a19f9e0771729f91be4a25ea4f4764a05066f
Chang Alan <alanchan@andrew.cmu.edu>
2015-03-29 15:13:08 -0400

    Parallax fixes

--- js/parallax.js
+++ js/parallax.js
@@ -9,13 +9,14 @@
 
         function updateParallax(initial) {
           var container_height;
-          if (window_width < 992) {
+          if (window_width < 601) {
             container_height = ($this.height() > 0) ? $this.height() : $this.children("img").height();
           }
           else {
             container_height = ($this.height() > 0) ? $this.height() : 500;
           }
-          var img_height = $this.children("img").height();
+          var $img = $this.children("img").first();
+          var img_height = $img.height();
           var parallax_dist = img_height - container_height;
           var bottom = $this.offset().top + container_height;
           var top = $this.offset().top;
@@ -26,10 +27,10 @@
           var parallax = Math.round((parallax_dist * percentScrolled));
 
           if (initial) {
-            $this.children("img").first().css('display', 'block');
+            $img.css('display', 'block');
           }
           if ((bottom > scrollTop) && (top < (scrollTop + windowHeight))) {
-            $this.children("img").first().css('transform', "translate3d(0," + parallax + "px,0)");
+            $img.css('transform', "translate3D(-50%," + parallax + "px, 0)");
           }
 
         }
