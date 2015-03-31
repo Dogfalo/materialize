950aa8cd1064dbcb5b4e1b0ed344c9a1cb0967f6
Chang Alan <alanchan@andrew.cmu.edu>
2015-03-31 13:54:28 -0400

    some color shade fixes

--- js/init.js
+++ js/init.js
@@ -8,9 +8,13 @@
       if (/^#[0-9A-F]{6}$/i.test(rgb)) { return rgb; }
 
       rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+      if (rgb === null) { return "N/A"; }
+
       function hex(x) {
           return ("0" + parseInt(x).toString(16)).slice(-2);
       }
+
       return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
     }
 
@@ -19,7 +23,7 @@
         var color = $(this).css('background-color'),
             classes = $(this).attr('class');
         $(this).html(rgb2hex(color) + " " + classes);
-        if (classes.indexOf("darken") >= 0) {
+        if (classes.indexOf("darken") >= 0 || $(this).hasClass('black')) {
           $(this).css('color', 'rgba(255,255,255,.9');
         }
       });
