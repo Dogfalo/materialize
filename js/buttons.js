454138b3b62e78926e0fdde5c896cd9e520bdcfe
Alvin Wang <alvin7@gmail.com>
2015-05-18 22:55:31 -0400

    Added methods to programmatically open/close FAB

--- js/buttons.js
+++ js/buttons.js
@@ -6,30 +6,52 @@
 
     $(document).on('mouseenter.fixedActionBtn', '.fixed-action-btn', function(e) {
       var $this = $(this);
-
-      $this.find('ul .btn-floating').velocity(
-        { scaleY: ".4", scaleX: ".4", translateY: "40px"},
-        { duration: 0 });
-
-      var time = 0;
-      $this.find('ul .btn-floating').reverse().each(function () {
-        $(this).velocity(
-          { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
-          { duration: 80, delay: time });
-        time += 40;
-      });
+      openFABMenu($this);
 
     });
 
     $(document).on('mouseleave.fixedActionBtn', '.fixed-action-btn', function(e) {
       var $this = $(this);
-
-      var time = 0;
-      $this.find('ul .btn-floating').velocity("stop", true);
-      $this.find('ul .btn-floating').velocity(
-        { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
-        { duration: 80 });
+      closeFABMenu($this);
     });
 
   });
+
+  $.fn.extend({
+    openFAB: function() {
+      var $this = $(this);
+      openFABMenu($this);
+    },
+    closeFAB: function() {
+      closeFABMenu($this);
+    }
+  });
+
+
+  var openFABMenu = function (btn) {
+    $this = btn;
+    $this.find('ul .btn-floating').velocity(
+      { scaleY: ".4", scaleX: ".4", translateY: "40px"},
+      { duration: 0 });
+
+    var time = 0;
+    $this.find('ul .btn-floating').reverse().each(function () {
+      $(this).velocity(
+        { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
+        { duration: 80, delay: time });
+      time += 40;
+    });
+  };
+
+  var closeFABMenu = function (btn) {
+    $this = btn;
+    var time = 0;
+    $this.find('ul .btn-floating').velocity("stop", true);
+    $this.find('ul .btn-floating').velocity(
+      { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
+      { duration: 80 }
+    );
+  };
+
+
 }( jQuery ));
