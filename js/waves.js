04b7247401a84f29930fc503af27994fb2ee7061
sergey <sergey@sam-media.com>
2015-05-25 11:09:49 +0800

    Support for js synthetic events

--- js/waves.js
+++ js/waves.js
@@ -72,15 +72,22 @@
 
             // Get click coordinate and element witdh
             var pos         = offset(el);
-            var relativeY   = (e.pageY - pos.top);
-            var relativeX   = (e.pageX - pos.left);
-            var scale       = 'scale('+((el.clientWidth / 100) * 10)+')';
-
+            var relativeY = 0;
+            var relativeX = 0;
             // Support for touch devices
-            if ('touches' in e) {
-              relativeY   = (e.touches[0].pageY - pos.top);
-              relativeX   = (e.touches[0].pageX - pos.left);
+            if('touches' in e && e.touches.length) {
+                relativeY   = (e.touches[0].pageY - pos.top);
+                relativeX   = (e.touches[0].pageX - pos.left);
             }
+            //Normal case
+            else {
+                relativeY   = (e.pageY - pos.top);
+                relativeX   = (e.pageX - pos.left);
+            }
+            // Support for synthetic events
+            relativeX = relativeX >= 0 ? relativeX : 0;
+            relativeY = relativeY >= 0 ? relativeY : 0;
+            var scale       = 'scale('+((el.clientWidth / 100) * 10)+')';
 
             // Attach data to element
             ripple.setAttribute('data-hold', Date.now());
