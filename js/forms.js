0f75bb5700af6148e364e37cb902dcc2ddade13b
Daniel Rix <Drainx1@live.com>
2015-07-15 10:15:18 -0500

    If the placeholder on an input is empty, we shouldn't classify that as active.
    Signed-off-by: Daniel Rix <Drainx1@live.com>

--- js/forms.js
+++ js/forms.js
@@ -5,7 +5,7 @@
     Materialize.updateTextFields = function() {
       var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
       $(input_selector).each(function(index, element) {
-        if ($(element).val().length > 0 || $(this).attr('placeholder') !== undefined || $(element)[0].validity.badInput === true) {
+        if ($(element).val().length > 0 || ($(this).attr('placeholder') !== undefined && $(this).attr('placeholder') !== "") || $(element)[0].validity.badInput === true) {
           $(this).siblings('label').addClass('active');
         }
         else {
