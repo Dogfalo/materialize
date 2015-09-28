3151c17d8825d6eabe8c0c9763abdcbf8199b51f
Alvin Wang <alvin7@gmail.com>
2015-09-27 22:11:57 -0700

    Added horizontal option to FAB

--- js/buttons.js
+++ js/buttons.js
@@ -31,15 +31,26 @@
   var openFABMenu = function (btn) {
     $this = btn;
     if ($this.hasClass('active') === false) {
+
+      // Get direction option
+      var horizontal = $this.hasClass('horizontal');
+      var offsetY, offsetX;
+      
+      if (horizontal === true) {
+        offsetX = 40;
+      } else {
+        offsetY = 40;
+      }
+      
       $this.addClass('active');
       $this.find('ul .btn-floating').velocity(
-        { scaleY: ".4", scaleX: ".4", translateY: "40px"},
+        { scaleY: ".4", scaleX: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px'},
         { duration: 0 });
 
       var time = 0;
-      $this.find('ul .btn-floating').reverse().each(function () {
+      $this.find('ul .btn-floating').reverse().each( function () {
         $(this).velocity(
-          { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
+          { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0", translateX: '0'},
           { duration: 80, delay: time });
         time += 40;
       });
@@ -48,11 +59,21 @@
 
   var closeFABMenu = function (btn) {
     $this = btn;
+    // Get direction option
+    var horizontal = $this.hasClass('horizontal');
+    var offsetY, offsetX;
+
+    if (horizontal === true) {
+      offsetX = 40;
+    } else {
+      offsetY = 40;
+    }
+
     $this.removeClass('active');
     var time = 0;
     $this.find('ul .btn-floating').velocity("stop", true);
     $this.find('ul .btn-floating').velocity(
-      { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
+      { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px'},
       { duration: 80 }
     );
   };
