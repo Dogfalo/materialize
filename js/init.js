e8bab8accc12a0ed84eddd9acbea7bb4f6f10d52
Chiharu Shibata <chi-bd@live.jp>
2015-11-21 12:39:40 +0900

    Add localization document with a sample for Japanese.

--- js/init.js
+++ js/init.js
@@ -128,7 +128,24 @@
     $('.modal-trigger').leanModal();
     $('.scrollspy').scrollSpy();
     $('.button-collapse').sideNav({'edge': 'left'});
-    $('.datepicker').pickadate({selectYears: 20});
+    $('#birthdate.datepicker').pickadate({selectYears: 20});
+    $('#birthdateHapanese.datepicker').pickadate({
+      selectMonths: true,
+      selectYears: 20,
+      monthsFull:  ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      monthsShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      weekdaysFull: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+      weekdaysShort:  ['日', '月', '火', '水', '木', '金', '土'],
+      weekdaysLetter: ['日', '月', '火', '水', '木', '金', '土'],
+      labelMonthNext: '翌月',
+      labelMonthPrev: '前月',
+      labelMonthSelect: '月を選択',
+      labelYearSelect: '年を選択',
+      today: '今日',
+      clear: 'クリア',
+      close: '閉じる',
+      format: 'yyyy/mm/dd'
+    });
     $('select').not('.disabled').material_select();
 
 
