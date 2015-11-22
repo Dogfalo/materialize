1d19ba7f7633656746e6c77c7d6adf2488835c0e
Chang Alan <alanchan@andrew.cmu.edu>
2015-11-21 17:51:20 -0800

    added test for multiple

--- tests/spec/select/selectSpec.js
+++ tests/spec/select/selectSpec.js
@@ -3,6 +3,7 @@ jasmine.getFixtures().fixturesPath = 'tests/spec';
 describe("Select Plugin", function () {
   beforeEach(function() {
     loadFixtures('select/selectFixture.html');
+    $('select').not('.disabled').material_select();
   });
 
   describe("Select", function () {
@@ -10,7 +11,6 @@ describe("Select Plugin", function () {
 
     beforeEach(function() {
       browserSelect = $('select.normal');
-      $('select').not('.disabled').material_select();
     });
 
     it("should open dropdown and select option", function (done) {
@@ -43,8 +43,43 @@ describe("Select Plugin", function () {
       normalDropdown = browserSelect.parent().find('ul.select-dropdown');
 
       var firstOption = browserSelect.find('option[selected]');
-      console.log(normalInput.val(), firstOption.text());
       expect(normalInput.val()).toEqual(firstOption.text(), 'Value should be equal to preselected option.');
     });
+  });
+
+  describe("Multiple Select", function () {
+    var browserSelect, multipleInput, multipleDropdown;
+
+    beforeEach(function() {
+      browserSelect = $('select.multiple');
+    });
+
+    it("should open dropdown and select multiple options", function(done) {
+      multipleInput = browserSelect.parent().find('input.select-dropdown');
+      multipleDropdown = browserSelect.parent().find('ul.select-dropdown');
+
+      expect(multipleInput).toExist('Should dynamically generate select dropdown structure.');
+      expect(multipleDropdown).toExist('Should dynamically generate select dropdown structure.');
+      expect(multipleInput).toBeVisible('Should be hidden before dropdown is opened.');
+      expect(multipleDropdown).toBeHidden('Should be hidden before dropdown is opened.');
+
+      multipleInput.click();
+
+      setTimeout(function() {
+        expect(multipleDropdown).toBeVisible('Should be visible after opening.');
+        var firstOption = multipleDropdown.find('li:not(.disabled)').first();
+        var thirdOption = multipleDropdown.find('li:not(.disabled)').eq(2);
+        firstOption.click();
+        thirdOption.click();
+        $('body').click();
+
+        setTimeout(function() {
+          expect(multipleDropdown).toBeHidden('Should be hidden after choosing item.');
+          expect(multipleInput.val()).toEqual(firstOption[0].innerText + ', ' + thirdOption[0].innerText, 'Value should equal chosen multiple options.');
+          done();
+        }, 400);
+      }, 400);
+    });
+
   });
 });
