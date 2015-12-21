cb8a7aa0a3191d30df60dbee89ab3434ac097921
Kevin Ndung'u <kevgathuku@gmail.com>
2015-12-21 10:09:07 +0300

    Finish Tests for ScrollFire Component
    Resolves #2482

--- tests/spec/scrollFire/scrollFireSpec.js
+++ tests/spec/scrollFire/scrollFireSpec.js
@@ -1,25 +1,31 @@
-describe('ScrollFire:', function() {
-  var options = [
-    {selector: '#test', offset: 50, callback: 'Materialize.toast("Scroll event fired!", 4000)' }
-  ];
+describe('ScrollFire Plugin', function() {
+  var options, testElement;
 
   beforeEach(function() {
+    options = [{
+      selector: '#test',
+      offset: 50,
+      callback: 'Materialize.callbackFunc()'
+    }];
+    Materialize.callbackFunc = function() {
+      return 'I have been called';
+    };
+
     loadFixtures('scrollFire/scrollFireFixture.html');
-    // testFixture.myTestedPlugin()
+    spyOn(Materialize, 'callbackFunc').and.callThrough();
+    testElement = $('#test');
+    Materialize.scrollFire(options);
   });
 
-  describe('scrollFire javascript functions', function() {
-    beforeEach(function() {
-      spyOn(Materialize, 'toast').and.callThrough();
-    });
-
+  describe('ScrollFire', function() {
     it('should fire the callback function', function(done) {
-      Materialize.scrollFire(options);
-      var testElement = document.getElementById("test");
       // Scroll to the test component
-      document.body.scrollTop = testElement.offsetTop;
-      expect(Materialize.toast).toHaveBeenCalledWith("Scroll event fired!", 4000);
-      done();
+      window.scrollTo(0, testElement.offset().top);
+      // The callback function should be fired after scrolling
+      setTimeout(function() {
+        expect(Materialize.callbackFunc).toHaveBeenCalled();
+        done();
+      }, 400);
     });
   });
 });
