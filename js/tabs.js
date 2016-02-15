3894a8e453d63b295fae667c7632c66a652072ef
Christopher Flanagan <chris@flana.com>
2016-02-14 20:24:14 -0500

    Add trigger for tab client event

--- js/tabs.js
+++ js/tabs.js
@@ -101,6 +101,9 @@
           $indicator.velocity({"right": $tabs_width - (($index + 1) * $tab_width)}, {duration: 300, queue: false, easing: 'easeOutQuad', delay: 90});
         }
 
+        // Trigger for any post-tab change handling
+        $('ul.tabs').trigger('tab_change', $active);
+
         // Prevent the anchor's default click action
         e.preventDefault();
       });
