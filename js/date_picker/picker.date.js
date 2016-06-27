567a1c5bfd8b79924e94ecce01d472ed33d6f49c
Ben Elliott <bengelliott@gmail.com>
2016-06-27 12:50:24 +0100

    Datepicker: add support for closeOnSelect and closeOnClear options

--- js/date_picker/picker.date.js
+++ js/date_picker/picker.date.js
@@ -1365,6 +1365,10 @@ DatePicker.defaults = (function( prefix ) {
         clear: 'Clear',
         close: 'Close',
 
+        // Picker close behavior (Prevent a change in behaviour for backwards compatibility)
+        closeOnSelect: false,
+        closeOnClear: false,
+
         // The format to show on the `input` element
         format: 'd mmmm, yyyy',
 
