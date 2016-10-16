7af9c3f8f52c9bfe02ae1d6c68c7b03791d366f2
forehalo <forehalo@gmail.com>
2016-10-16 10:35:43 +0800

    update checking for jquery strategy
    note that users always define or require `jQuery` and `$` in  global `window` variable, but in some context, 'jQuery' not representing `window.jQuery`. This make sure materialize and other library use the same instance of jquery but not init materialize's own which may cuz some conflict.

--- js/initial.js
+++ js/initial.js
@@ -1,8 +1,13 @@
 // Check for jQuery.
 if (typeof(jQuery) === 'undefined') {
+
   var jQuery;
+  // Projects with zone.js will only have jQuery in the window object
+  if (typeof(window.jQuery) !== 'undefined') {
+    jQuery = $ = window.jQuery;
+  }
   // Check if require is a defined function.
-  if (typeof(require) === 'function') {
+  else if (typeof(require) === 'function') {
     jQuery = $ = require('jquery');
   // Else use the dollar sign alias.
   } else {
