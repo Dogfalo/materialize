fda1e45c1ca38dabd12143e595d0b01f4f9f4858
Chang Alan <alanchan@andrew.cmu.edu>
2016-10-30 13:58:36 -0700

    v0.97.8 Release

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'materialize:materialize',  // http://atmospherejs.com/materialize/materialize
   summary: 'Materialize (official): A modern responsive front-end framework based on Material Design',
-  version: '0.97.7',
+  version: '0.97.8',
   git: 'https://github.com/Dogfalo/materialize.git'
 });
 
