297442c6891f4bc76a61387d531a75311fb3777e
wackychocolatefactory <jacob@mulquin.com>
2016-12-15 23:33:14 +1100

    Reimplement closeOnSelect functionality

--- js/date_picker/picker.js
+++ js/date_picker/picker.js
@@ -300,6 +300,9 @@ function PickerConstructor( ELEMENT, NAME, COMPONENT, OPTIONS ) {
                             // On “enter”, if the highlighted item isn’t disabled, set the value and close.
                             else if ( !P.$root.find( '.' + CLASSES.highlighted ).hasClass( CLASSES.disabled ) ) {
                                 P.set( 'select', P.component.item.highlight ).close()
+                                if ( SETTINGS.closeOnSelect ) {
+                                    P.close( true )
+                                }
                             }
                         }
 
@@ -716,6 +719,9 @@ function PickerConstructor( ELEMENT, NAME, COMPONENT, OPTIONS ) {
                 // If something is picked, set `select` then close with focus.
                 else if ( !targetDisabled && 'pick' in targetData ) {
                     P.set( 'select', targetData.pick )
+                    if ( SETTINGS.closeOnSelect ) {
+                        P.close( true )
+                    }
                 }
 
                 // If a “clear” button is pressed, empty the values and close with focus.
