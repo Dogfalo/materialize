a2fcbfd56183e09c31464f4c6851d63eecc915b2
Chang Alan <alanchan@andrew.cmu.edu>
2017-01-25 12:17:32 -0800

    v0.98.0 Release

--- extras/noUiSlider/nouislider.js
+++ extras/noUiSlider/nouislider.js
@@ -1,5 +1,5 @@
 /*!
- * Materialize v0.97.8 (http://materializecss.com)
+ * Materialize v0.98.0 (http://materializecss.com)
  * Copyright 2014-2015 Materialize
  * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
  */
