bf01e95514495684d90754a16ba8eed25f0f074b
Chang Alan <alanchan@andrew.cmu.edu>
2017-02-06 16:34:11 -0800

    cleaned up js and fixed up responsiveness

--- js/tapTarget.js
+++ js/tapTarget.js
@@ -4,7 +4,6 @@
   init: function (options) {
     return this.each(function() {
     var origin = $('#'+$(this).attr('data-activates'));
-    var originIcon = origin.find('i');
     var screen = $('body');
 
     // Creating tap target
@@ -12,7 +11,6 @@
     var tapTargetWrapper = tapTargetEl.parent('.tap-target-wrapper');
     var tapTargetWave = tapTargetWrapper.find('.tap-target-wave');
     var tapTargetOriginEl = tapTargetWrapper.find('.tap-target-origin');
-    var tapTargetOriginIconEl = tapTargetOriginEl.find('i');
     var tapTargetContentEl = tapTargetEl.find('.tap-target-content');
 
     // Creating wrapper
@@ -28,13 +26,11 @@
 
     // Creating foreground wave
     if (!tapTargetWave.length) {
-      console.log("APPEND WAVE", tapTargetWave);
       tapTargetWave = $('<div class="tap-target-wave"></div>');
 
       // Creating origin
       if (!tapTargetOriginEl.length) {
         tapTargetOriginEl = origin.clone(true, true);
-        tapTargetOriginIconEl = tapTargetOriginEl.find('i');
         tapTargetOriginEl.addClass('tap-target-origin');
         tapTargetOriginEl.removeAttr('id');
         tapTargetWave.append(tapTargetOriginEl);
@@ -45,18 +41,12 @@
 
     // Open
     var openTapTarget = function() {
-      console.log("OPEN");
-      if (tapTargetWrapper.is('.open'))
+      if (tapTargetWrapper.is('.open')) {
         return;
+      }
 
       // Adding open class
       tapTargetWrapper.addClass('open');
-
-      // Animating
-      tapTargetEl.velocity('stop');
-      // tapTargetEl.velocity({scale: [1, 0]}, { duration: duration, queue: false });
-
-      // tapTargetWaveEl.velocity({ scale: [1, 0.8] }, { duration: duration, loop: true });
 
       setTimeout(function() {
         tapTargetOriginEl.off('click.tapTarget').on('click.tapTarget', function(e) {
@@ -73,12 +63,11 @@
 
     // Close
     var closeTapTarget = function(){
-      console.log("CLOSE");
-      if (!tapTargetWrapper.is('.open'))
-      return;
+      if (!tapTargetWrapper.is('.open')) {
+        return;
+      }
 
       tapTargetWrapper.removeClass('open');
-
       tapTargetOriginEl.off('click.tapTarget')
       $(document).off('click.tapTarget');
     };
@@ -86,14 +75,15 @@
     // Pre calculate
     var calculateTapTarget = function() {
       // Element or parent is fixed position?
-      var isFixed = origin.css('position') == 'fixed';
+      var isFixed = origin.css('position') === 'fixed';
       if (!isFixed) {
-          var parents = origin.parents();
-          for(var i=0; i<parents.length; i++) {
-              isFixed = $(parents[i]).css('position') == 'fixed';
-              if (isFixed)
-                  break;
+        var parents = origin.parents();
+        for(var i = 0; i < parents.length; i++) {
+          isFixed = $(parents[i]).css('position') == 'fixed';
+          if (isFixed) {
+            break;
           }
+        }
       }
 
       // Calculating origin
@@ -103,16 +93,16 @@
       var originLeft = origin.offset().left;
 
       // Calculating screen
-      var screenWidth = $(window).prop('innerWidth');
-      var screenHeight = $(window).prop('innerHeight');
-      var centerX = screenWidth / 2;
-      var centerY = screenHeight / 2;
+      var windowWidth = window.innerWidth;
+      var windowHeight = window.innerHeight;
+      var centerX = windowWidth / 2;
+      var centerY = windowHeight / 2;
       var isLeft = originLeft <= centerX;
       var isRight = originLeft > centerX;
       var isTop = originTop <= centerY;
       var isBottom = originTop > centerY;
-      var isCenterX = originLeft >= screenWidth*0.25 && originLeft <= screenWidth*0.75;
-      var isCenterY = originTop >= screenHeight*0.25 && originTop <= screenHeight*0.75;
+      var isCenterX = originLeft >= windowWidth*0.25 && originLeft <= windowWidth*0.75;
+      var isCenterY = originTop >= windowHeight*0.25 && originTop <= windowHeight*0.75;
 
       // Calculating tap target
       var tapTargetWidth = tapTargetEl.outerWidth();
@@ -137,54 +127,35 @@
       var tapTargetWaveTop = tapTargetHeight/2 - tapTargetWaveHeight/2;
       var tapTargetWaveLeft = tapTargetWidth/2 - tapTargetWaveWidth/2;
 
-      // Calculationg origin
-      var tapTargetOriginWidth = tapTargetOriginEl.outerWidth();
-      var tapTargetOriginHeight = tapTargetOriginEl.outerHeight();
-      var tapTargetOriginTop = tapTargetHeight/2 - tapTargetOriginHeight/2;
-      var tapTargetOriginLeft = tapTargetWidth/2 - tapTargetOriginWidth/2;
-      var tapTargetOriginIconElColor = !origin.is('.btn') && originIcon.css('color') == tapTargetWave.css('backgroundColor') ? tapTargetEl.css('backgroundColor') : '';
-
       // Setting tap target
-      tapTargetWrapper.css({
-      top: tapTargetTop,
-      left: tapTargetLeft,
-      position: tapTargetPosition
-      });
+      var tapTargetWrapperCssObj = {};
+      tapTargetWrapperCssObj.top = isTop ? tapTargetTop : '';
+      tapTargetWrapperCssObj.right = isRight ? windowWidth - tapTargetLeft - tapTargetWidth : '';
+      tapTargetWrapperCssObj.bottom = isBottom ? windowHeight - tapTargetTop - tapTargetHeight : '';
+      tapTargetWrapperCssObj.left = isLeft ? tapTargetLeft : '';
+      tapTargetWrapperCssObj.position = tapTargetPosition;
+      tapTargetWrapper.css(tapTargetWrapperCssObj);
 
       // Setting content
       tapTargetContentEl.css({
-      width: tapTargetTextWidth,
-      height: tapTargetTextHeight,
-      top: tapTargetTextTop,
-      right: tapTargetTextRight,
-      bottom: tapTargetTextBottom,
-      left: tapTargetTextLeft,
-      padding: tapTargetTextPadding,
-      verticalAlign: tapTargetTextAlign
+        width: tapTargetTextWidth,
+        height: tapTargetTextHeight,
+        top: tapTargetTextTop,
+        right: tapTargetTextRight,
+        bottom: tapTargetTextBottom,
+        left: tapTargetTextLeft,
+        padding: tapTargetTextPadding,
+        verticalAlign: tapTargetTextAlign
       });
 
       // Setting wave
       tapTargetWave.css({
-      top: tapTargetWaveTop,
-      left: tapTargetWaveLeft,
-      width: tapTargetWaveWidth,
-      height: tapTargetWaveHeight
+        top: tapTargetWaveTop,
+        left: tapTargetWaveLeft,
+        width: tapTargetWaveWidth,
+        height: tapTargetWaveHeight
       });
-
-      // Setting origin
-      // tapTargetOriginEl.css({
-      // top: tapTargetOriginTop,
-      // left: tapTargetOriginLeft
-      // });
-      tapTargetOriginIconEl.css('color', tapTargetOriginIconElColor);
     }
-
-    // Resize
-    $(window).off('resize.tapTarget').on('resize.tapTarget', function() {
-      if (tapTargetEl.is('.open')) {
-        calculateTapTarget();
-      }
-    });
 
     if (options == 'open') {
       calculateTapTarget();
@@ -195,8 +166,8 @@
       closeTapTarget();
     });
   },
-  open: function(){},
-  close: function(){}
+  open: function() {},
+  close: function() {}
   };
 
   $.fn.tapTarget = function(methodOrOptions) {
