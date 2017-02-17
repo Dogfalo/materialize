d537f34c18b7846b31163d057d68759b83a43751
Chang Alan <alanchan@andrew.cmu.edu>
2017-02-16 22:14:47 -0800

    reverted init

--- js/init.js
+++ js/init.js
@@ -180,7 +180,6 @@
     $('select').not('.disabled').material_select();
     $('input.autocomplete').autocomplete({
       data: {"Apple": null, "Microsoft": null, "Google": 'http://placehold.it/250x250'},
-      minLength: 0
     });
 
     // Chips
