87f00bf7855e4b01d785872216da9de2776f81aa
Daniel Ruf <DanielRuf@users.noreply.github.com>
2017-04-26 15:47:24 +0200

    remove wrong parentheses
    Weird, the previous Travis builds were successful.

--- js/chips.js
+++ js/chips.js
@@ -208,7 +208,7 @@
       if (label.length) {
         label.attr('for', chipId);
 
-        if (($chips.data('chips')!== undefined && $chips.data('chips').length) {
+        if ($chips.data('chips')!== undefined && $chips.data('chips').length) {
           label.addClass('active');
         }
       }
@@ -240,7 +240,7 @@
     };
 
     this.setPlaceholder = function($chips) {
-      if (($chips.data('chips') !== undefined && $chips.data('chips').length && curr_options.placeholder) {
+      if ($chips.data('chips') !== undefined && $chips.data('chips').length && curr_options.placeholder) {
         $chips.find('input').prop('placeholder', curr_options.placeholder);
 
       } else if (($chips.data('chips') === undefined || !$chips.data('chips').length) && curr_options.secondaryPlaceholder) {
@@ -313,4 +313,3 @@
     this.handleEvents();
   };
 }( jQuery ));
-// end
