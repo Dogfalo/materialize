ca150500bb24cf27aadf713407aa5fae1e7c34d8
Arno Roldao Junior <arno.junior@getnet.com.br>
2017-05-03 16:20:44 -0300

    Putting the COMPRESS settings inside a .JS file

---
+++ grunt-tasks/compress.js
@@ -0,0 +1,89 @@
+'use strict';
+
+module.exports = function (grunt) {
+    return {
+        main: {
+            options: {
+                archive: 'bin/materialize.zip',
+                level: 6
+            },
+            files: [
+                { expand: true, cwd: 'dist/', src: ['**/*'], dest: 'materialize/' },
+                { expand: true, cwd: './', src: ['LICENSE', 'README.md'], dest: 'materialize/' },
+            ]
+        },
+
+        src: {
+            options: {
+                archive: 'bin/materialize-src.zip',
+                level: 6
+            },
+            files: [
+                { expand: true, cwd: 'font/', src: ['**/*'], dest: 'materialize-src/font/' },
+                { expand: true, cwd: 'sass/', src: ['materialize.scss'], dest: 'materialize-src/sass/' },
+                { expand: true, cwd: 'sass/', src: ['components/**/*'], dest: 'materialize-src/sass/' },
+                {
+                    expand: true, cwd: 'js/', src: [
+                        "jquery.easing.1.3.js",
+                        "animation.js",
+                        "velocity.min.js",
+                        "hammer.min.js",
+                        "jquery.hammer.js",
+                        "collapsible.js",
+                        "dropdown.js",
+                        "leanModal.js",
+                        "materialbox.js",
+                        "parallax.js",
+                        "tabs.js",
+                        "tooltip.js",
+                        "waves.js",
+                        "toasts.js",
+                        "sideNav.js",
+                        "scrollspy.js",
+                        "forms.js",
+                        "slider.js",
+                        "cards.js",
+                        "pushpin.js",
+                        "buttons.js",
+                        "transitions.js",
+                        "scrollFire.js",
+                        "date_picker/picker.js",
+                        "date_picker/picker.date.js",
+                    ], dest: 'materialize-src/js/'
+                },
+                { expand: true, cwd: 'dist/js/', src: ['**/*'], dest: 'materialize-src/js/bin/' },
+                { expand: true, cwd: './', src: ['LICENSE', 'README.md'], dest: 'materialize-src/' }
+
+            ]
+        },
+
+        starter_template: {
+            options: {
+                archive: 'templates/starter-template.zip',
+                level: 6
+            },
+            files: [
+                { expand: true, cwd: 'dist/', src: ['**/*'], dest: 'starter-template/' },
+                { expand: true, cwd: 'templates/starter-template/', src: ['index.html', 'LICENSE'], dest: 'starter-template/' },
+                { expand: true, cwd: 'templates/starter-template/css', src: ['style.css'], dest: 'starter-template/css' },
+                { expand: true, cwd: 'templates/starter-template/js', src: ['init.js'], dest: 'starter-template/js' }
+
+            ]
+        },
+
+        parallax_template: {
+            options: {
+                archive: 'templates/parallax-template.zip',
+                level: 6
+            },
+            files: [
+                { expand: true, cwd: 'dist/', src: ['**/*'], dest: 'parallax-template/' },
+                { expand: true, cwd: 'templates/parallax-template/', src: ['index.html', 'LICENSE', 'background1.jpg', 'background2.jpg', 'background3.jpg'], dest: 'parallax-template/' },
+                { expand: true, cwd: 'templates/parallax-template/css', src: ['style.css'], dest: 'parallax-template/css' },
+                { expand: true, cwd: 'templates/parallax-template/js', src: ['init.js'], dest: 'parallax-template/js' }
+
+            ]
+        }
+
+    }
+};
