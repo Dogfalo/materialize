33dba19110bf0d54012665188094ac390ba8b523
Tom Scholz <tomscholz@outlook.com>
2017-05-10 10:47:26 +0200

    This should fix the tests…. In theory

--- tests/spec/tabs/tabsSpec.js
+++ tests/spec/tabs/tabsSpec.js
@@ -73,14 +73,16 @@ describe("Tabs Plugin", function () {
     });
 
 
-    it("should horizontally scroll when too many tabs", function () {
+    it("should horizontally scroll when too many tabs", function (done) {
       var tabsScrollWidth = 0;
       normalTabs.parent().css('width', '400px');
       normalTabs.find('.tab').each(function() {
         tabsScrollWidth += $(this).width();
       });
-
-      expect(tabsScrollWidth).toBeGreaterThan(normalTabs.width(), 'Scroll width should exceed tabs width');
+      setTimeout(function() {
+        expect(tabsScrollWidth).toBeGreaterThan(normalTabs.width(), 'Scroll width should exceed tabs width');
+        done();
+      }, 400);
     });
 
   });
