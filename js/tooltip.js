9da65524cb26792d857ae0437b2d27fdc0a4d0db
jeroenqui <jeroenqui@gmail.com>
2017-06-27 08:18:01 +0200

    Remove 'data-tooltip-id' attribute from the origin on tooltip('remove')
    Do not only remove the tooltip content element, but also the 'data-tooltip-id' attribute from the origin.

--- js/tooltip.js
+++ js/tooltip.js
@@ -15,6 +15,7 @@
       if (options === "remove") {
         this.each(function() {
           $('#' + $(this).attr('data-tooltip-id')).remove();
+          $(this).removeAttr('data-tooltip-id');
           $(this).off('mouseenter.tooltip mouseleave.tooltip');
         });
         return false;
