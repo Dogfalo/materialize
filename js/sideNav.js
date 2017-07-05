63a10a9548c3ef4ca6a701f25268bc1b2f53c414
Chang Alan <alanchan@andrew.cmu.edu>
2017-07-05 14:18:53 -0700

    Removed unnecessary onOpen

--- js/sideNav.js
+++ js/sideNav.js
@@ -186,12 +186,12 @@
                 $overlay.css('opacity', 0).click( function(){
                   removeMenu();
                 });
-                
+
                 // Run 'onOpen' when sidenav is opened via touch/swipe if applicable
                 if (typeof(options.onOpen) === 'function') {
                   options.onOpen.call(this, menu);
                 }
-                
+
                 $('body').append($overlay);
               }
 
@@ -280,7 +280,7 @@
                       if (typeof(options.onClose) === 'function') {
                         options.onClose.call(this, menu);
                       }
-                      
+
                       $(this).remove();
                     }});
                   $dragTarget.css({width: '10px', right: '', left: 0});
@@ -312,7 +312,7 @@
                       if (typeof(options.onClose) === 'function') {
                         options.onClose.call(this, menu);
                       }
-                      
+
                       $(this).remove();
                     }});
                   $dragTarget.css({width: '10px', right: 0, left: ''});
@@ -350,28 +350,27 @@
               menu.velocity({'translateX': [0, options.menuWidth]}, {duration: 300, queue: false, easing: 'easeOutQuad'});
             }
 
+            // Overlay close on click
             $overlay.css('opacity', 0)
-            .click(function(){
-              menuOut = false;
-              panning = false;
-              removeMenu();
-              $overlay.velocity({opacity: 0}, {duration: 300, queue: false, easing: 'easeOutQuad',
-                complete: function() {
-                  $(this).remove();
-                } });
-                // Run 'onOpen' when sidenav is opened via touch/swipe if applicable
-                if (typeof(options.onOpen) === 'function') {
-                  options.onOpen.call(this, menu);
-                }
+              .click(function() {
+                menuOut = false;
+                panning = false;
+                removeMenu();
+                $overlay.velocity({opacity: 0}, {duration: 300, queue: false, easing: 'easeOutQuad',
+                  complete: function() {
+                    $(this).remove();
+                  }
+                });
+              });
 
-              });
-              $('body').append($overlay);
-              $overlay.velocity({opacity: 1}, {duration: 300, queue: false, easing: 'easeOutQuad',
-                complete: function () {
-                  menuOut = true;
-                  panning = false;
-                }
-              });
+            // Append body
+            $('body').append($overlay);
+            $overlay.velocity({opacity: 1}, {duration: 300, queue: false, easing: 'easeOutQuad',
+              complete: function () {
+                menuOut = true;
+                panning = false;
+              }
+            });
 
             // Callback
             if (typeof(options.onOpen) === 'function') {
