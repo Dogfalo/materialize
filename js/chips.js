2acea468624f55370a1a15eb27c580d9db83454b
Chang Alan <alanchan@andrew.cmu.edu>
2017-07-10 18:23:17 -0700

    Reversed order of placehodler and secondary placeholder in chips

--- js/chips.js
+++ js/chips.js
@@ -243,10 +243,10 @@
     };
 
     this.setPlaceholder = function($chips) {
-      if ($chips.data('chips') !== undefined && $chips.data('chips').length && curr_options.placeholder) {
+      if (($chips.data('chips') !== undefined && !$chips.data('chips').length) && curr_options.placeholder) {
         $chips.find('input').prop('placeholder', curr_options.placeholder);
 
-      } else if (($chips.data('chips') === undefined || !$chips.data('chips').length) && curr_options.secondaryPlaceholder) {
+      } else if (($chips.data('chips') === undefined || !!$chips.data('chips').length) && curr_options.secondaryPlaceholder) {
         $chips.find('input').prop('placeholder', curr_options.secondaryPlaceholder);
       }
     };
