f1119ed724f4f8ef408398211d2e31ca703a7bac
Alvin Wang <alvin7@gmail.com>
2017-07-17 19:34:59 -0700

    wip: toast rewrite

--- js/toasts.js
+++ js/toasts.js
@@ -1,136 +1,169 @@
-Materialize.toast = function (message, displayLength, className, completeCallback) {
-  className = className || "";
+(function() {
+  'use strict';
+  // Materialize.toast = function (message, displayLength, className, completeCallback) {
 
-  var container = document.getElementById('toast-container');
-
-  // Create toast container if it does not exist
-  if (container === null) {
-    // create notification container
-    container = document.createElement('div');
-    container.id = 'toast-container';
-    document.body.appendChild(container);
-  }
-
-  // Select and append toast
-  var newToast = createToast(message);
-
-  // only append toast if message is not undefined
-  if(message){
-    container.appendChild(newToast);
-  }
-
-  newToast.style.opacity = 0;
-
-  // Animate toast in
-  Vel(newToast, {translateY: '-35px',  opacity: 1 }, {duration: 300,
-    easing: 'easeOutCubic',
-    queue: false});
-
-  // Allows timer to be pause while being panned
-  var timeLeft = displayLength;
-  var counterInterval;
-  if (timeLeft != null)  {
-    counterInterval = setInterval (function(){
-      if (newToast.parentNode === null)
-        window.clearInterval(counterInterval);
-
-      // If toast is not being dragged, decrease its time remaining
-      if (!newToast.classList.contains('panning')) {
-        timeLeft -= 20;
+  class Toast {
+    constructor(message, displayLength, className, completeCallback) {
+      if (!message) {
+        return;
       }
 
-      if (timeLeft <= 0) {
-        // Animate toast out
-        Vel(newToast, {"opacity": 0, marginTop: '-40px'}, { duration: 375,
-            easing: 'easeOutExpo',
-            queue: false,
-            complete: function(){
-              // Call the optional callback
-              if(typeof(completeCallback) === "function")
-                completeCallback();
-              // Remove toast after it times out
-              this[0].parentNode.removeChild(this[0]);
-            }
-          });
-        window.clearInterval(counterInterval);
+      this.message = message;
+      this.options = {
+        displayLength: displayLength,
+        inDuration: 300,
+        outDuration: 375,
+        className: className,
+        completeCallback: completeCallback,
+        activationPercent: 0.8
+      };
+
+      if (Toast._count === 0) {
+        Toast.createContainer();
       }
-    }, 20);
-  }
 
+      Toast._count++;
+      let toastElement = this.createToast();
+      toastElement.M_Toast = this;
+      this.el = toastElement;
 
+    }
 
-  function createToast(html) {
+    static createContainer() {
+      let container = document.createElement('div');
+      container.setAttribute('id', 'toast-container');
 
-    // Create toast
-    var toast = document.createElement('div');
-    toast.classList.add('toast');
-    if (className) {
-      var classes = className.split(' ');
+      // Add event handler
+      container.addEventListener('touchstart', Toast._onDragStart);
+      container.addEventListener('touchmove', Toast._onDragMove);
+      container.addEventListener('touchend', Toast._onDragEnd);
 
-      for (var i = 0, count = classes.length; i < count; i++) {
-        toast.classList.add(classes[i]);
+      document.body.appendChild(container);
+      Toast._container = container;
+    }
+
+    static _onDragStart(e) {
+      if (e.target && e.target.classList.contains('toast')) {
+        let toast = e.target.M_Toast;
+        e.target.style.transition = null;
+        toast.startingXPos = Toast._xPos(e);
+        toast.time = Date.now();
+        toast.xPos = Toast._xPos(e);
       }
     }
-  // If type of parameter is HTML Element
-    if ( typeof HTMLElement === "object" ? html instanceof HTMLElement : html && typeof html === "object" && html !== null && html.nodeType === 1 && typeof html.nodeName==="string"
-) {
-      toast.appendChild(html);
+
+    static _onDragMove(e) {
+      if (e.target && e.target.classList.contains('toast')) {
+        let toast = e.target.M_Toast;
+        toast.deltaX = Math.abs(toast.xPos - Toast._xPos(e));
+        toast.xPos = Toast._xPos(e);
+        toast.velocityX = toast.deltaX / (Date.now() - toast.time);
+        toast.time = Date.now();
+
+        let totalDeltaX = toast.xPos - toast.startingXPos;
+        let activationDistance =
+            e.target.offsetWidth * toast.options.activationPercent;
+        e.target.style.transform = `translateX(${totalDeltaX}px)`;
+        e.target.style.opacity = 1-Math.abs(totalDeltaX / activationDistance);
+      }
     }
-    else if (html instanceof jQuery) {
+
+    static _onDragEnd(e) {
+      if (e.target && e.target.classList.contains('toast')) {
+        let toast = e.target.M_Toast;
+        let totalDeltaX = toast.xPos - toast.startingXPos;
+        let activationDistance =
+            e.target.offsetWidth * toast.options.activationPercent;
+        let shouldBeDismissed = Math.abs(totalDeltaX) > activationDistance ||
+            toast.velocityX > 1;
+        if (shouldBeDismissed) {
+          toast.remove();
+
+        } else {
+          e.target.style.transition = 'transform .2s, opacity .2s';
+          e.target.style.transform = null;
+          e.target.style.opacity = null;
+        }
+      }
+    }
+
+    static _xPos(e) {
+      if (e.targetTouches && (e.targetTouches.length >= 1)) {
+        return e.targetTouches[0].clientX;
+      }
+      // mouse event
+      return e.clientX;
+    }
+
+    createToast() {
+      let toast = document.createElement('div');
+      toast.classList.add('toast');
+
+
+      // Add custom classes onto toast
+      if (this.options.className) {
+        var classes = className.split(' ');
+        for (var i = 0, count = classes.length; i < count; i++) {
+          toast.classList.add(classes[i]);
+        }
+      }
+
+      // Set content
+      if ( typeof HTMLElement === "object" ?
+           this.message instanceof HTMLElement :
+           this.message && typeof this.message === "object" &&
+           this.message !== null && this.message.nodeType === 1 &&
+           typeof this.message.nodeName==="string"
+         ) {
+        toast.appendChild(this.message);
+
       // Check if it is jQuery object
-      toast.appendChild(html[0]);
-    }
-    else {
-      // Insert as text;
-      toast.innerHTML = html;
-    }
-    // Bind hammer
-    var hammerHandler = new Hammer(toast, {prevent_default: false});
-    hammerHandler.on('pan', function(e) {
-      var deltaX = e.deltaX;
-      var activationDistance = 80;
+      } else if (this.message instanceof jQuery) {
+        toast.appendChild(this.message[0]);
 
-      // Change toast state
-      if (!toast.classList.contains('panning')){
-        toast.classList.add('panning');
-      }
-
-      var opacityPercent = 1-Math.abs(deltaX / activationDistance);
-      if (opacityPercent < 0)
-        opacityPercent = 0;
-
-      Vel(toast, {left: deltaX, opacity: opacityPercent }, {duration: 50, queue: false, easing: 'easeOutQuad'});
-
-    });
-
-    hammerHandler.on('panend', function(e) {
-      var deltaX = e.deltaX;
-      var activationDistance = 80;
-
-      // If toast dragged past activation point
-      if (Math.abs(deltaX) > activationDistance) {
-        Vel(toast, {marginTop: '-40px'}, { duration: 375,
-          easing: 'easeOutExpo',
-          queue: false,
-          complete: function(){
-            if(typeof(completeCallback) === "function") {
-              completeCallback();
-            }
-            toast.parentNode.removeChild(toast);
-          }
-        });
-
+        // Insert as text;
       } else {
-        toast.classList.remove('panning');
-        // Put toast back into original position
-        Vel(toast, { left: 0, opacity: 1 }, { duration: 300,
-          easing: 'easeOutExpo',
-          queue: false
-        });
-
+        toast.innerHTML = this.message;
       }
-    });
 
-    return toast;
+      // Append toast
+      Toast._container.appendChild(toast);
+
+      return toast;
+    }
+
+    remove() {
+      let activationDistance =
+          this.el.offsetWidth * this.options.activationPercent;
+
+      this.el.style.transition = 'transform .05s, opacity .05s';
+      this.el.style.transform = `translateX(${activationDistance}px)`;
+      this.el.style.opacity = 0;
+      this.el.parentNode.removeChild(this.el);
+
+      Toast._count--;
+      if (Toast._count === 0) {
+        Toast._container.parentNode.removeChild(Toast._container);
+      }
+    }
   }
-};
+
+  /**
+   * @static
+   * @memberof Toast
+   */
+  Toast._count = 0;
+
+
+  /**
+   * @static
+   * @memberof Toast
+   */
+  Toast._container = undefined;
+
+  window.Materialize.Toast = Toast;
+  window.Materialize.toast = function(message, displayLength, className, completeCallback) {
+    return new Toast(message, displayLength, className, completeCallback);
+  }
+// };
+})();
