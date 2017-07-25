6cb9deabf0d5c591a9056d3c48dc60f8558fc61e
Chang Alan <alanchan@andrew.cmu.edu>
2017-07-24 17:11:14 -0700

    started conversion

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,431 @@
 (function ($) {
+  'use strict';
+
+  let _defaults = {
+    duration: 200, // ms
+    dist: -100, // zoom scale TODO: make this more intuitive as an option
+    shift: 0, // spacing for center image
+    padding: 0, // Padding between non center items
+    fullWidth: false, // Change to full width styles
+    indicators: false, // Toggle indicators
+    noWrap: false, // Don't wrap around and cycle through items.
+    onCycleTo: null // Callback for when a new slide is cycled to.
+  }
+
+
+  /**
+   * @class
+   *
+   */
+  class Carousel {
+    /**
+     * Construct Carousel instance
+     * @constructor
+     * @param {jQuery} $el
+     * @param {Object} options
+     */
+    constructor($el, options) {
+
+      // If exists, destroy and reinitialize
+      if (!!$el[0].M_Carousel) {
+        $el[0].M_Carousel.destroy();
+      }
+
+      /**
+       * The jQuery element
+       * @type {jQuery}
+       */
+      this.$el = $el;
+
+      /**
+       * Options for the carousel
+       * @member Carousel#options
+       * @prop {Number} duration
+       * @prop {Number} dist
+       * @prop {number} shift
+       * @prop {number} padding
+       * @prop {Boolean} fullWidth
+       * @prop {Boolean} indicators
+       * @prop {Boolean} noWrap
+       * @prop {Function} onCycleTo
+       */
+      this.options = $.extend({}, Carousel.defaults, options);
+
+      this.$el[0].M_Carousel = this;
+
+      // Setup
+      this.hasMultipleSlides = this.$el.find('.carousel-item').length > 1;
+      this.showIndicators = this.options.indicators && hasMultipleSlides;
+      this.noWrap = this.options.noWrap || !hasMultipleSlides;
+      this.pressed = false;
+      this.offset = this.target = 0;
+      // images = [];
+      this.itemWidth = view.find('.carousel-item').first().innerWidth();
+      this.itemHeight = view.find('.carousel-item').first().innerHeight();
+      this.dim = this.itemWidth * 2 + this.options.padding;
+
+      this._setupEventHandlers();
+    }
+
+    static get defaults() {
+      return _defaults;
+    }
+
+    static init($els, options) {
+      let arr = [];
+      $els.each(function() {
+        arr.push(new Carousel($(this), options));
+      });
+      return arr;
+    }
+
+    /**
+     * Get Instance
+     */
+    getInstance() {
+      return this;
+    }
+
+    /**
+     * Teardown component
+     */
+    destroy() {
+      this._removeEventHandlers();
+      this.$el[0].M_Carousel = undefined;
+    }
+
+    /**
+     * Setup Event Handlers
+     */
+    _setupEventHandlers() {
+      this._handleCarouselTapBound = this._handleCarouselTap.bind(this);
+      this._handleCarouselReleaseBound = this._handleCarouselRelease.bind(this);
+
+      if (typeof window.ontouchstart !== 'undefined') {
+        this.$el[0].addEventListener('touchstart', this._handleCarouselTapBound);
+        this.$el[0].addEventListener('touchmove', this._handleCarouselDragBound);
+        this.$el[0].addEventListener('touchmove', this._handleCarouselReleaseBound);
+        view.on('touchend.carousel', release);
+      }
+      view.on('mousedown.carousel', tap);
+      view.on('mousemove.carousel', drag);
+      view.on('mouseup.carousel', release);
+      view.on('mouseleave.carousel', release);
+      view.on('click.carousel', click);
+      this.$el[0].addEventListener('click', this._handleCollapsibleClickBound);
+    }
+
+    /**
+     * Remove Event Handlers
+     */
+    _removeEventHandlers() {
+      this.$el[0].removeEventListener('click', this._handleCollapsibleClickBound);
+    }
+
+    /**
+     * Handle Carousel Tap
+     * @param {Event} e
+     */
+    _handleCarouselTap(e) {
+      // Fixes firefox draggable image bug
+      if (e.type === 'mousedown' && $(e.target).is('img')) {
+        e.preventDefault();
+      }
+      this.pressed = true;
+      this.dragged = false;
+      this.verticalDragged = false;
+      this.reference = this._xpos(e);
+      this.referenceY = this._ypos(e);
+
+      this.velocity = this.amplitude = 0;
+      this.frame = this.offset;
+      this.timestamp = Date.now();
+      clearInterval(this.ticker);
+      this.ticker = setInterval(track, 100);
+    }
+
+    /**
+     * Handle Carousel Drag
+     * @param {Event} e
+     */
+    _handleCarouselDrag(e) {
+      let x, delta, deltaY;
+      if (this.pressed) {
+        x = this._xpos(e);
+        y = this._ypos(e);
+        delta = this.reference - x;
+        deltaY = Math.abs(this.referenceY - y);
+        if (deltaY < 30 && !this.verticalDragged) {
+          // If vertical scrolling don't allow dragging.
+          if (delta > 2 || delta < -2) {
+            this.dragged = true;
+            this.reference = x;
+            scroll(offset + delta);
+          }
+
+        } else if (this.dragged) {
+          // If dragging don't allow vertical scroll.
+          e.preventDefault();
+          e.stopPropagation();
+          return false;
+
+        } else {
+          // Vertical scrolling.
+          this.verticalDragged = true;
+        }
+      }
+
+      if (this.dragged) {
+        // If dragging don't allow vertical scroll.
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+      }
+    }
+
+    /**
+     * Handle Carousel Release
+     * @param {Event} e
+     */
+    _handleCarouselRelease(e) {
+      if (this.pressed) {
+        this.pressed = false;
+      } else {
+        return;
+      }
+
+      clearInterval(this.ticker);
+      this.target = this.offset;
+      if (this.velocity > 10 || this.velocity < -10) {
+        this.amplitude = 0.9 * this.velocity;
+        this.target = this.offset + this.amplitude;
+      }
+      this.target = Math.round(this.target / this.dim) * this.dim;
+
+      // No wrap of items.
+      if (noWrap) {
+        if (target >= dim * (count - 1)) {
+          target = dim * (count - 1);
+        } else if (target < 0) {
+          target = 0;
+        }
+      }
+      amplitude = target - offset;
+      timestamp = Date.now();
+      requestAnimationFrame(autoScroll);
+
+      if (dragged) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      return false;
+    }
+
+    /**
+     * Get x position from event
+     * @param {Event} e
+     */
+    _xpos(e) {
+      // touch event
+      if (e.targetTouches && (e.targetTouches.length >= 1)) {
+        return e.targetTouches[0].clientX;
+      }
+
+      // mouse event
+      return e.clientX;
+    }
+
+    /**
+     * Get y position from event
+     * @param {Event} e
+     */
+    _ypos(e) {
+      // touch event
+      if (e.targetTouches && (e.targetTouches.length >= 1)) {
+        return e.targetTouches[0].clientY;
+      }
+
+      // mouse event
+      return e.clientY;
+    }
+
+    /**
+     * Wrap index
+     * @param {Number} x
+     */
+    _wrap(x) {
+      return (x >= this.count) ? (x % this.count) : (x < 0) ? wrap(this.count + (x % this.count)) : x;
+    }
+
+    /**
+     * Tracks scrolling information
+     */
+    _track() {
+      let now, elapsed, delta, v;
+
+      now = Date.now();
+      elapsed = now - timestamp;
+      this.timestamp = now;
+      delta = this.offset - this.frame;
+      this.frame = this.offset;
+
+      v = 1000 * delta / (1 + elapsed);
+      this.velocity = 0.8 * v + 0.2 * this.velocity;
+    }
+
+    /**
+     * Auto scrolls to nearest carousel item.
+     */
+    _autoScroll() {
+      let elapsed, delta;
+
+      if (this.amplitude) {
+        elapsed = Date.now() - this.timestamp;
+        delta = this.amplitude * Math.exp(-elapsed / this.options.duration);
+        if (delta > 2 || delta < -2) {
+          this._scroll(this.target - delta);
+          requestAnimationFrame(this._autoScroll);
+        } else {
+          this._scroll(this.target);
+        }
+      }
+    }
+
+    /**
+     * Scroll to target
+     * @param {Number} x
+     */
+    _scroll(x) {
+      // Track scrolling state
+      if (!this.$el.hasClass('scrolling')) {
+        this.$el.addClass('scrolling');
+      }
+      if (this.scrollingTimeout != null) {
+        window.clearTimeout(this.scrollingTimeout);
+      }
+      this.scrollingTimeout = window.setTimeout(function() {
+        this.$el.removeClass('scrolling');
+      }, this.options.duration);
+
+      // Start actual scroll
+      let i, half, delta, dir, tween, el, alignment, xTranslation;
+      let lastCenter = center;
+
+      this.offset = (typeof x === 'number') ? x : this.offset;
+      this.center = Math.floor((this.offset + this.dim / 2) / this.dim);
+      delta = this.offset - this.center * this.dim;
+      dir = (delta < 0) ? 1 : -1;
+      tween = -dir * delta * 2 / this.dim;
+      half = this.count >> 1;
+
+      if (!options.fullWidth) {
+        alignment = 'translateX(' + (view[0].clientWidth - item_width) / 2 + 'px) ';
+        alignment += 'translateY(' + (view[0].clientHeight - item_height) / 2 + 'px)';
+      } else {
+        alignment = 'translateX(0)';
+      }
+
+      // Set indicator active
+      if (showIndicators) {
+        var diff = (center % count);
+        var activeIndicator = $indicators.find('.indicator-item.active');
+        if (activeIndicator.index() !== diff) {
+          activeIndicator.removeClass('active');
+          $indicators.find('.indicator-item').eq(diff).addClass('active');
+        }
+      }
+
+      // center
+      // Don't show wrapped items.
+      if (!noWrap || (center >= 0 && center < count)) {
+        el = images[wrap(center)];
+
+        // Add active class to center item.
+        if (!$(el).hasClass('active')) {
+          view.find('.carousel-item').removeClass('active');
+          $(el).addClass('active');
+        }
+        el.style[xform] = alignment +
+          ' translateX(' + (-delta / 2) + 'px)' +
+          ' translateX(' + (dir * options.shift * tween * i) + 'px)' +
+          ' translateZ(' + (options.dist * tween) + 'px)';
+        el.style.zIndex = 0;
+        if (options.fullWidth) { tweenedOpacity = 1; }
+        else { tweenedOpacity = 1 - 0.2 * tween; }
+        el.style.opacity = tweenedOpacity;
+        el.style.display = 'block';
+      }
+
+      for (i = 1; i <= half; ++i) {
+        // right side
+        if (options.fullWidth) {
+          zTranslation = options.dist;
+          tweenedOpacity = (i === half && delta < 0) ? 1 - tween : 1;
+        } else {
+          zTranslation = options.dist * (i * 2 + tween * dir);
+          tweenedOpacity = 1 - 0.2 * (i * 2 + tween * dir);
+        }
+        // Don't show wrapped items.
+        if (!noWrap || center + i < count) {
+          el = images[wrap(center + i)];
+          el.style[xform] = alignment +
+            ' translateX(' + (options.shift + (dim * i - delta) / 2) + 'px)' +
+            ' translateZ(' + zTranslation + 'px)';
+          el.style.zIndex = -i;
+          el.style.opacity = tweenedOpacity;
+          el.style.display = 'block';
+        }
+
+
+        // left side
+        if (options.fullWidth) {
+          zTranslation = options.dist;
+          tweenedOpacity = (i === half && delta > 0) ? 1 - tween : 1;
+        } else {
+          zTranslation = options.dist * (i * 2 - tween * dir);
+          tweenedOpacity = 1 - 0.2 * (i * 2 - tween * dir);
+        }
+        // Don't show wrapped items.
+        if (!noWrap || center - i >= 0) {
+          el = images[wrap(center - i)];
+          el.style[xform] = alignment +
+            ' translateX(' + (-options.shift + (-dim * i - delta) / 2) + 'px)' +
+            ' translateZ(' + zTranslation + 'px)';
+          el.style.zIndex = -i;
+          el.style.opacity = tweenedOpacity;
+          el.style.display = 'block';
+        }
+      }
+
+      // center
+      // Don't show wrapped items.
+      if (!noWrap || (center >= 0 && center < count)) {
+        el = images[wrap(center)];
+        el.style[xform] = alignment +
+          ' translateX(' + (-delta / 2) + 'px)' +
+          ' translateX(' + (dir * options.shift * tween) + 'px)' +
+          ' translateZ(' + (options.dist * tween) + 'px)';
+        el.style.zIndex = 0;
+        if (options.fullWidth) { tweenedOpacity = 1; }
+        else { tweenedOpacity = 1 - 0.2 * tween; }
+        el.style.opacity = tweenedOpacity;
+        el.style.display = 'block';
+      }
+
+      // onCycleTo callback
+      if (lastCenter !== center &&
+          typeof(options.onCycleTo) === "function") {
+        var $curr_item = view.find('.carousel-item').eq(wrap(center));
+        options.onCycleTo.call(this, $curr_item, dragged);
+      }
+
+      // One time callback
+      if (typeof(oneTimeCallback) === "function") {
+        oneTimeCallback.call(this, $curr_item, dragged);
+        oneTimeCallback = null;
+      }
+    }
+  }
 
   var methods = {
 
@@ -134,29 +561,29 @@
           view.on('click.carousel', click);
         }
 
-        function xpos(e) {
-          // touch event
-          if (e.targetTouches && (e.targetTouches.length >= 1)) {
-            return e.targetTouches[0].clientX;
-          }
+        // function xpos(e) {
+        //   // touch event
+        //   if (e.targetTouches && (e.targetTouches.length >= 1)) {
+        //     return e.targetTouches[0].clientX;
+        //   }
 
-          // mouse event
-          return e.clientX;
-        }
+        //   // mouse event
+        //   return e.clientX;
+        // }
 
-        function ypos(e) {
-          // touch event
-          if (e.targetTouches && (e.targetTouches.length >= 1)) {
-            return e.targetTouches[0].clientY;
-          }
+        // function ypos(e) {
+        //   // touch event
+        //   if (e.targetTouches && (e.targetTouches.length >= 1)) {
+        //     return e.targetTouches[0].clientY;
+        //   }
 
-          // mouse event
-          return e.clientY;
-        }
+        //   // mouse event
+        //   return e.clientY;
+        // }
 
-        function wrap(x) {
-          return (x >= count) ? (x % count) : (x < 0) ? wrap(count + (x % count)) : x;
-        }
+        // function wrap(x) {
+        //   return (x >= count) ? (x % count) : (x < 0) ? wrap(count + (x % count)) : x;
+        // }
 
         function scroll(x) {
           // Track scrolling state
@@ -361,58 +788,58 @@
           }
         }
 
-        function tap(e) {
-          // Fixes firefox draggable image bug
-          if (e.type === 'mousedown' && $(e.target).is('img')) {
-            e.preventDefault();
-          }
-          pressed = true;
-          dragged = false;
-          vertical_dragged = false;
-          reference = xpos(e);
-          referenceY = ypos(e);
+        // function tap(e) {
+        //   // Fixes firefox draggable image bug
+        //   if (e.type === 'mousedown' && $(e.target).is('img')) {
+        //     e.preventDefault();
+        //   }
+        //   pressed = true;
+        //   dragged = false;
+        //   vertical_dragged = false;
+        //   reference = xpos(e);
+        //   referenceY = ypos(e);
 
-          velocity = amplitude = 0;
-          frame = offset;
-          timestamp = Date.now();
-          clearInterval(ticker);
-          ticker = setInterval(track, 100);
-        }
+        //   velocity = amplitude = 0;
+        //   frame = offset;
+        //   timestamp = Date.now();
+        //   clearInterval(ticker);
+        //   ticker = setInterval(track, 100);
+        // }
 
-        function drag(e) {
-          var x, delta, deltaY;
-          if (pressed) {
-            x = xpos(e);
-            y = ypos(e);
-            delta = reference - x;
-            deltaY = Math.abs(referenceY - y);
-            if (deltaY < 30 && !vertical_dragged) {
-              // If vertical scrolling don't allow dragging.
-              if (delta > 2 || delta < -2) {
-                dragged = true;
-                reference = x;
-                scroll(offset + delta);
-              }
+        // function drag(e) {
+        //   var x, delta, deltaY;
+        //   if (pressed) {
+        //     x = xpos(e);
+        //     y = ypos(e);
+        //     delta = reference - x;
+        //     deltaY = Math.abs(referenceY - y);
+        //     if (deltaY < 30 && !vertical_dragged) {
+        //       // If vertical scrolling don't allow dragging.
+        //       if (delta > 2 || delta < -2) {
+        //         dragged = true;
+        //         reference = x;
+        //         scroll(offset + delta);
+        //       }
 
-            } else if (dragged) {
-              // If dragging don't allow vertical scroll.
-              e.preventDefault();
-              e.stopPropagation();
-              return false;
+        //     } else if (dragged) {
+        //       // If dragging don't allow vertical scroll.
+        //       e.preventDefault();
+        //       e.stopPropagation();
+        //       return false;
 
-            } else {
-              // Vertical scrolling.
-              vertical_dragged = true;
-            }
-          }
+        //     } else {
+        //       // Vertical scrolling.
+        //       vertical_dragged = true;
+        //     }
+        //   }
 
-          if (dragged) {
-            // If dragging don't allow vertical scroll.
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
-          }
-        }
+        //   if (dragged) {
+        //     // If dragging don't allow vertical scroll.
+        //     e.preventDefault();
+        //     e.stopPropagation();
+        //     return false;
+        //   }
+        // }
 
         function release(e) {
           if (pressed) {
