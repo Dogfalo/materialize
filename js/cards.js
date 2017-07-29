691b337ae85f4cfff9dbb63565d77588f6a82704
neko2016 <honglok16@hotmail.com>
2017-07-29 18:50:27 +0100

    add keyboard access so that users can use ENTER key to reveal and close a card component

--- js/cards.js
+++ js/cards.js
@@ -1,36 +1,36 @@
 (function ($) {
   $(document).ready(function() {
-
-    $(document).on('click.card', '.card', function (e) {
-      if ($(this).find('> .card-reveal').length) {
-        var $card = $(e.target).closest('.card');
-        if ($card.data('initialOverflow') === undefined) {
-          $card.data(
-            'initialOverflow',
-            $card.css('overflow') === undefined ? '' : $card.css('overflow')
-          );
-        }
-        if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
-          // Make Reveal animate down and display none
-          $(this).find('.card-reveal').velocity(
-            {translateY: 0}, {
-              duration: 225,
-              queue: false,
-              easing: 'easeInOutQuad',
-              complete: function() {
-                $(this).css({ display: 'none'});
-                $card.css('overflow', $card.data('initialOverflow'));
+    $(document).on('keypress click', '.card', function (e) {
+      if (e.which === 13 || e.type === 'click'){
+        if ($(this).find('> .card-reveal').length) {
+          var $card = $(e.target).closest('.card');
+          if ($card.data('initialOverflow') === undefined) {
+            $card.data(
+              'initialOverflow',
+              $card.css('overflow') === undefined ? '' : $card.css('overflow')
+            );
+          }
+          if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
+            // Make Reveal animate down and display none
+            $(this).find('.card-reveal').velocity(
+              {translateY: 0}, {
+                duration: 225,
+                queue: false,
+                easing: 'easeInOutQuad',
+                complete: function() {
+                  $(this).css({ display: 'none'});
+                  $card.css('overflow', $card.data('initialOverflow'));
+                }
               }
-            }
-          );
-        }
-        else if ($(e.target).is($('.card .activator')) ||
-                 $(e.target).is($('.card .activator i')) ) {
-          $card.css('overflow', 'hidden');
-          $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
+            );
+          }
+          else if ($(e.target).is($('.card .activator')) ||
+                   $(e.target).is($('.card .activator i')) ) {
+            $card.css('overflow', 'hidden');
+            $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
+          }
         }
       }
     });
-
   });
 }( jQuery ));
