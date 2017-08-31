b8140093c2b64cf3c3f19159bf15885b937f8d26
Dogfalo7 <alvin7@gmail.com>
2017-08-30 23:53:35 -0700

    class style fixes for parallax

--- js/parallax.js
+++ js/parallax.js
@@ -6,48 +6,36 @@
 
   class Parallax {
 
-    constructor($el, options) {
+    constructor(el, options) {
       // If exists, destroy and reinitialize
-      if (!!$el[0].M_Parallax) {
-        $el[0].M_Parallax.destroy();
+      if (!!el.M_Parallax) {
+        el.M_Parallax.destroy();
       }
 
-      this.$el = $el;
-      this.el = $el[0];
-      this.options = $.extend({}, Parallax.defaults, options);
-
+      this.el = el;
+      this.$el = $(el);
       this.$el[0].M_Parallax = this;
 
-      this.$img = $(this.el).children('img').first();
+      this.options = $.extend({}, Parallax.defaults, options);
 
-      this.updateParallax();
+      this.$img = $(this.el).children('img').first();
+      this._updateParallax();
       this._setupEventHandlers();
       this._setupStyles();
 
       Parallax._parallaxes.push(this);
-
-
     }
 
     static get defaults() {
       return _defaults;
     }
 
-
     static init($els, options) {
       let arr = [];
       $els.each(function() {
-        arr.push(new Parallax($(this), options));
+        arr.push(new Parallax(this, options));
       });
       return arr;
-    }
-
-
-    static _handleScroll() {
-      for(let i = 0; i < Parallax._parallaxes.length; i++) {
-        let parallaxInstance = Parallax._parallaxes[i];
-        parallaxInstance.updateParallax.call(parallaxInstance);
-      }
     }
 
     /**
@@ -57,6 +45,19 @@
       return this;
     }
 
+    /**
+     * Teardown component
+     */
+    destroy() {
+
+    }
+
+    static _handleScroll() {
+      for(let i = 0; i < Parallax._parallaxes.length; i++) {
+        let parallaxInstance = Parallax._parallaxes[i];
+        parallaxInstance._updateParallax.call(parallaxInstance);
+      }
+    }
 
     _setupEventHandlers() {
       this._handleImageLoadBound = this._handleImageLoad.bind(this);
@@ -68,22 +69,19 @@
       }
     }
 
-
     _setupStyles() {
       this.$img[0].style.opacity = 1;
     }
 
-
     _handleImageLoad() {
-      updateParallax();
+      this._updateParallax();
       this.$img.each(function() {
         let el = this;
         if (el.complete) $(el).trigger("load");
       });
     }
 
-
-    updateParallax() {
+    _updateParallax() {
       let containerHeight = this.$el.height() > 0 ? this.el.parentNode.offsetHeight : 500;
       let imgHeight = this.$img[0].offsetHeight;
       let parallaxDist = imgHeight - containerHeight;
@@ -99,16 +97,6 @@
         this.$img[0].style.transform = `translate3D(-50%, ${parallax}px, 0)`;
       }
     }
-
-
-    /**
-     * Teardown component
-     */
-    destroy() {
-
-    }
-
-
   }
 
   /**
@@ -117,7 +105,7 @@
    */
   Parallax._parallaxes = [];
 
-  window.Materialize.Parallax = Parallax;
+  Materialize.Parallax = Parallax;
 
   jQuery.fn.parallax = function(methodOrOptions) {
     // Call plugin method if valid method name is passed in
