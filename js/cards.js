dcdff71685ed5f2be591a636c4c4aff82ed61196
Chang Alan <alanchan@andrew.cmu.edu>
2017-10-19 17:44:12 -0700

    Removed jQuery from cards

--- js/cards.js
+++ js/cards.js
@@ -1,8 +1,8 @@
-(function ($) {
+(function ($, Vel) {
   $(document).ready(function() {
 
-    $(document).on('click.card', '.card', function (e) {
-      if ($(this).find('> .card-reveal').length) {
+    $(document).on('click', '.card', function (e) {
+      if ($(this).children('.card-reveal').length) {
         var $card = $(e.target).closest('.card');
         if ($card.data('initialOverflow') === undefined) {
           $card.data(
@@ -10,10 +10,13 @@
             $card.css('overflow') === undefined ? '' : $card.css('overflow')
           );
         }
+        let $cardReveal = $(this).find('.card-reveal');
         if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
           // Make Reveal animate down and display none
-          $(this).find('.card-reveal').velocity(
-            {translateY: 0}, {
+          Vel(
+            $cardReveal,
+            {translateY: 0},
+            {
               duration: 225,
               queue: false,
               easing: 'easeInOutQuad',
@@ -27,10 +30,16 @@
         else if ($(e.target).is($('.card .activator')) ||
                  $(e.target).is($('.card .activator i')) ) {
           $card.css('overflow', 'hidden');
-          $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
+          $cardReveal.css({ display: 'block'});
+          Vel($cardReveal, 'stop');
+          Vel(
+            $cardReveal,
+            {translateY: '-100%'},
+            {duration: 300, queue: false, easing: 'easeInOutQuad'}
+          );
         }
       }
     });
 
   });
-}( jQuery ));
+}( cash, Materialize.Vel));
