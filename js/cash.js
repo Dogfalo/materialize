5e26168a33ac782b5cd657d6cbfaa2acffdf26fa
Dogfalo7 <alvin7@gmail.com>
2017-11-02 13:30:14 -0700

    Bind cash to window

--- js/cash.js
+++ js/cash.js
@@ -1,7 +1,7 @@
 /*! cash-dom 1.3.5, https://github.com/kenwheeler/cash @license MIT */
-(function (root, factory) {
-  root.cash = factory();
-})(this, function () {
+(function (factory) {
+  window.cash = factory();
+})(function () {
   var doc = document, win = window, ArrayProto = Array.prototype, slice = ArrayProto.slice, filter = ArrayProto.filter, push = ArrayProto.push;
 
   var noop = function () {}, isFunction = function (item) {
