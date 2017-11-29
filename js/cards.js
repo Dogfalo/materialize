1cc8ce574799cc39e4f5569059efef4c182cb720
Dogfalo7 <alvin7@gmail.com>
2017-11-28 20:26:43 -0800

    Update cards and tabs with anime.js

--- js/cards.js
+++ js/cards.js
@@ -1,4 +1,4 @@
-(function ($, Vel) {
+(function ($, anim) {
   $(document).on('click', '.card', function (e) {
     if ($(this).children('.card-reveal').length) {
       var $card = $(e.target).closest('.card');
@@ -11,31 +11,29 @@
       let $cardReveal = $(this).find('.card-reveal');
       if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
         // Make Reveal animate down and display none
-        Vel(
-          $cardReveal,
-          {translateY: 0},
-          {
-            duration: 225,
-            queue: false,
-            easing: 'easeInOutQuad',
-            complete: function() {
-              $(this).css({ display: 'none'});
-              $card.css('overflow', $card.data('initialOverflow'));
-            }
+        anim({
+          targets: $cardReveal[0],
+          translateY: 0,
+          duration: 225,
+          easing: 'easeInOutQuad',
+          complete: function(anim) {
+            let el = anim.animatables[0].target;
+            $(el).css({ display: 'none'});
+            $card.css('overflow', $card.data('initialOverflow'));
           }
-        );
+        });
       }
       else if ($(e.target).is($('.card .activator')) ||
                $(e.target).is($('.card .activator i')) ) {
         $card.css('overflow', 'hidden');
         $cardReveal.css({ display: 'block'});
-        Vel($cardReveal, 'stop');
-        Vel(
-          $cardReveal,
-          {translateY: '-100%'},
-          {duration: 300, queue: false, easing: 'easeInOutQuad'}
-        );
+        anim({
+          targets: $cardReveal[0],
+          translateY: '-100%',
+          duration: 300,
+          easing: 'easeInOutQuad'
+        });
       }
     }
   });
-}( cash, M.Vel));
+}( cash, anime));
