e9e2943e74999bad510185cdb15b247a090ae327
Giannis Mathioudakis <mathiou.gian@gmail.com>
2017-12-27 14:24:52 +0200

    [Collections]: Add support for striped colors such as in tables. Issue #5490. Updated collections_content and latest build.

--- dist/js/materialize.js
+++ dist/js/materialize.js
@@ -1,4 +1,9 @@
 /*!
+ * Materialize vundefined (http://materializecss.com)
+ * Copyright 2014-2017 Materialize
+ * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
+ */
+/*!
  * Materialize v1.0.0-alpha.2 (http://materializecss.com)
  * Copyright 2014-2017 Materialize
  * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
