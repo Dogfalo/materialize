03c3fd805a42a3a0b752ac0421c41f476ab5b31f
ray007 <ray007@gmail.com>
2018-01-19 21:24:06 +0100

    Datepicker formats fix (#5573)
    * fix formatters for "dd", "mm" and "yy", add formatters "d" and "m"
    
    * update docs for new and changed datepicker formats
    
    * use full month names for format "mmmm"

--- js/datepicker.js
+++ js/datepicker.js
@@ -656,8 +656,12 @@
 
       this.formats = {
 
-        dd: () => {
+        d: () => {
           return this.date.getDate();
+        },
+        dd: () => {
+          let d = this.date.getDate();
+          return (d < 10 ? '0' : '') + d;
         },
         ddd: () => {
           return this.options.i18n.weekdaysShort[this.date.getDay()];
@@ -665,17 +669,21 @@
         dddd: () => {
           return this.options.i18n.weekdays[this.date.getDay()];
         },
-        mm: () => {
+        m: () => {
           return this.date.getMonth() + 1;
+        },
+        mm: () => {
+          let m = this.date.getMonth() + 1;
+          return (m < 10 ? '0' : '') + m;
         },
         mmm: () => {
           return this.options.i18n.monthsShort[this.date.getMonth()];
         },
         mmmm: () => {
-          return this.options.i18n.monthsShort[this.date.getMonth()];
+          return this.options.i18n.months[this.date.getMonth()];
         },
         yy: () => {
-          return this.date.getFullYear().slice(2);
+          return ('' + this.date.getFullYear()).slice(2);
         },
         yyyy: () => {
           return this.date.getFullYear();
