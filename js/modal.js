fb6499735af7c1aad18f43df16afa6355c67dcba
Alvin Wang <alvin7@gmail.com>
2018-02-18 15:47:24 -0800

    Added modalsOpen counter to better allow nested modals

--- js/modal.js
+++ js/modal.js
@@ -57,10 +57,7 @@
       this._openingTrigger = undefined;
       this.$overlay = $('<div class="modal-overlay"></div>');
 
-      Modal._increment++;
       Modal._count++;
-      this.$overlay[0].style.zIndex = 1000 + Modal._increment * 2;
-      this.el.style.zIndex = 1000 + Modal._increment * 2 + 1;
       this._setupEventHandlers();
     }
 
@@ -278,6 +275,11 @@
       }
 
       this.isOpen = true;
+      Modal._modalsOpen++;
+
+      // Set Z-Index based on number of currently open modals
+      this.$overlay[0].style.zIndex = 1000 + Modal._modalsOpen * 2;
+      this.el.style.zIndex = 1000 + Modal._modalsOpen * 2 + 1;
 
       // Set opening trigger, undefined indicates modal was opened by javascript
       this._openingTrigger = !!$trigger ? $trigger[0] : undefined;
@@ -311,6 +313,7 @@
       }
 
       this.isOpen = false;
+      Modal._modalsOpen--;
 
       // Call onCloseStart callback
       if (typeof(this.options.onCloseStart) === 'function') {
@@ -320,7 +323,7 @@
       this.el.classList.remove('open');
 
       // Enable body scrolling only if there are no more modals open.
-      if (Modal._count === 0) {
+      if (Modal._modalsOpen === 0) {
         document.body.style.overflow = '';
       }
 
@@ -339,7 +342,7 @@
    * @static
    * @memberof Modal
    */
-  Modal._increment = 0;
+  Modal._modalsOpen = 0;
 
   /**
    * @static
