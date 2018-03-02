26e2723998b0f5363eeb02531ef81950c59da419
Chang Alan <alanchan@andrew.cmu.edu>
2018-03-01 16:00:04 -0800

    fixed out of bounds error when deleting first chip

--- js/chips.js
+++ js/chips.js
@@ -186,7 +186,9 @@
           let index = currChips._selectedChip.index();
           currChips.deleteChip(index);
           currChips._selectedChip = null;
-          selectIndex = index - 1;
+
+          // Make sure selectIndex doesn't go negative
+          selectIndex = Math.max(index - 1, 0);
         }
 
         if (currChips.chipsData.length) {
