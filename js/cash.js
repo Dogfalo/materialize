3f061c0ee542da7bb7b27efdbd42c71df69b02f9
ginlime <webmaster@emeraldshell.com>
2018-05-14 09:22:33 +0900

    Reverted UMD as original according to #5912
    See #5912

--- js/cash.js
+++ js/cash.js
@@ -1,7 +1,13 @@
 /*! cash-dom 1.3.5, https://github.com/kenwheeler/cash @license MIT */
-(function (factory) {
-  window.cash = factory();
-})(function () {
+;(function (root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(factory);
+  } else if (typeof exports !== "undefined") {
+    module.exports = factory();
+  } else {
+    root.cash = root.$ = factory();
+  }
+})(this, function () {
   var doc = document, win = window, ArrayProto = Array.prototype, slice = ArrayProto.slice, filter = ArrayProto.filter, push = ArrayProto.push;
 
   var noop = function () {}, isFunction = function (item) {
