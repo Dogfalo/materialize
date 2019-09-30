2e3dbf392d6111903407229c435859ce16905120
Dogfalo7 <alvin7@gmail.com>
2019-09-30 10:15:17 -0700

    update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,8 @@
 module.exports = {
-  extends: [
-    'airbnb-base',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module' // Allows for the use of imports
   },
   plugins: ['prettier'],
   rules: {
@@ -13,14 +10,18 @@ module.exports = {
     'no-multiple-empty-lines': 'error',
     'import/extensions': 'off',
     'import/prefer-default-export': 'off',
-    "react/state-in-constructor": "off",
-    "react/prop-types": "off",
   },
   settings: {
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
       }
     }
+  },
+  globals: {
+    $: 'readonly'
+  },
+  env: {
+    jasmine: true
   }
 };
