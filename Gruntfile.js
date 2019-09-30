43ccf3eaea040b3c8070ff8cc6bc717753b2d429
Dogfalo7 <alvin7@gmail.com>
2019-09-29 19:09:55 -0700

    Update travis config

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,21 +4,21 @@ module.exports = function(grunt) {
   // configure the tasks
   let config = {
     //  Jasmine
-    jasmine: {
-      components: {
-        src: ['bin/materialize.js'],
-        options: {
-          vendor: [
-            'node_modules/jquery/dist/jquery.min.js',
-            'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
-          ],
-          styles: 'bin/materialize.css',
-          specs: 'tests/spec/**/*Spec.js',
-          helpers: 'tests/spec/helper.js',
-          keepRunner: true
-        }
-      }
-    },
+    // jasmine: {
+    //   components: {
+    //     src: ['bin/materialize.js'],
+    //     options: {
+    //       vendor: [
+    //         'node_modules/jquery/dist/jquery.min.js',
+    //         'node_modules/jasmine-jquery/lib/jasmine-jquery.js'
+    //       ],
+    //       styles: 'bin/materialize.css',
+    //       specs: 'tests/spec/**/*Spec.js',
+    //       helpers: 'tests/spec/helper.js',
+    //       keepRunner: true
+    //     }
+    //   }
+    // },
 
     //  Sass
     sass: {
