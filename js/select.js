9bcd9c11fdea90f8db7ca87a80f454a6af3231f3
Adrienne Tacke <adriennetacke@users.noreply.github.com>
2019-10-23 14:31:33 -0700

    Revert "fixed select bug where onOpenEnd was being overwritten in dropdownOptions"
    This reverts commit 61e8ee98a231042602490dc2916ff3180dc5572f.

--- js/select.js
+++ js/select.js
@@ -234,7 +234,6 @@
       // Initialize dropdown
       if (!this.el.disabled) {
         let dropdownOptions = $.extend({}, this.options.dropdownOptions);
-        let userOnOpenEnd = dropdownOptions.onOpenEnd;
 
         // Add callback for centering selected option when dropdown content is scrollable
         dropdownOptions.onOpenEnd = (el) => {
@@ -257,11 +256,6 @@
               scrollOffset -= this.dropdownOptions.clientHeight / 2; // center in dropdown
               this.dropdownOptions.scrollTop = scrollOffset;
             }
-          }
-
-          // Handle user declared onOpenEnd if needed
-          if (userOnOpenEnd) {
-            userOnOpenEnd.call(this.dropdown, this.el);
           }
         };
 
