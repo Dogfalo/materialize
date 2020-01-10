fad0bc89ca771a33b13ec9e068fa119b7af33a79
Martin Minka <martin.minka@gmail.com>
2020-01-10 17:26:44 +0100

    make sure to close year and month dropdowns on datepicker close

--- js/datepicker.js
+++ js/datepicker.js
@@ -234,6 +234,17 @@
       }
     }
 
+    closeSelects() {
+      let oldYearSelect = this.calendarEl.querySelector('.orig-select-year');
+      if (oldYearSelect) {
+        M.FormSelect.getInstance(oldYearSelect).dropdown.close();
+      }
+      let oldMonthSelect = this.calendarEl.querySelector('.orig-select-month');
+      if (oldMonthSelect) {
+        M.FormSelect.getInstance(oldMonthSelect).dropdown.close();
+      }
+    }
+
     _insertHTMLIntoDOM() {
       if (this.options.showClearBtn) {
         $(this.clearBtn).css({ visibility: '' });
@@ -253,6 +264,9 @@
     _setupModal() {
       this.modalEl.id = 'modal-' + this.id;
       this.modal = M.Modal.init(this.modalEl, {
+        onCloseStart: () => {
+          this.closeSelects();
+        },
         onCloseEnd: () => {
           this.isOpen = false;
         }
@@ -513,9 +527,7 @@
       }
       return (
         `<td data-day="${opts.day}" class="${arr.join(' ')}" aria-selected="${ariaSelected}">` +
-        `<button class="datepicker-day-button" type="button" data-year="${opts.year}" data-month="${
-          opts.month
-        }" data-day="${opts.day}">${opts.day}</button>` +
+        `<button class="datepicker-day-button" type="button" data-year="${opts.year}" data-month="${opts.month}" data-day="${opts.day}">${opts.day}</button>` +
         '</td>'
       );
     }
