34dce64a71e4b04bf5a962239c8c9e1c618710c6
Allie <13716824+ChildishGiant@users.noreply.github.com>
2021-04-29 14:28:48 +0100

    ci: ensure conventional commit styles are used for PRs
    See #79. Also bump version in package-lock

---
+++ commitlint.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  helpUrl: "Confused? Try 'npm run-script commit'. For more info: https://github.com/conventional-changelog/commitlint/#what-is-commitlint"
+}
