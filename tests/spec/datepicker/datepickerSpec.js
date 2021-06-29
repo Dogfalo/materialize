3931749b0a20d73d3f92794bb6d434ea79881b7a
Calamari <georg@tavonius.de>
2021-06-29 13:17:24 +0200

    feat(datepicker): add format method
    To leverace browser own internationalization methods, it is useful
    to add a format method instead of simple format string. That makes
    things like this possible:
    
    ```js
    M.Datepicker.init(elems, {
      format: date => new Intl.DateTimeFormat(locale).format(date)
    });
    ```
    
    Which would format the input according to the i18n rules defined
    by the browser using the locale of the user.

--- tests/spec/datepicker/datepickerSpec.js
+++ tests/spec/datepicker/datepickerSpec.js
@@ -15,8 +15,8 @@ describe('Datepicker Plugin', function() {
     });
 
     it('should open and close programmatically', function(done) {
-      let input = document.querySelector('#datepickerInput');
-      let modal = document.querySelector('.datepicker-modal');
+      const input = document.querySelector('#datepickerInput');
+      const modal = document.querySelector('.datepicker-modal');
 
       expect(modal).toBeHidden('Should be hidden before datepicker input is focused.');
 
@@ -34,6 +34,54 @@ describe('Datepicker Plugin', function() {
             'open',
             'Datepicker modal should be hidden after datepicker input is focused.'
           );
+          done();
+        }, 400);
+      }, 400);
+    });
+
+    it('can have a string format', function(done) {
+      const input = document.querySelector('#datepickerInput');
+
+      const today = new Date();
+
+      M.Datepicker.init(input, { format: 'mm/dd/yyyy' }).open();
+      M.Datepicker.getInstance(input).open();
+
+      setTimeout(function() {
+        const day1 = document.querySelector('.datepicker-modal button[data-day="1"]');
+        day1.click();
+
+        setTimeout(function() {
+          const year = today.getFullYear();
+          let month = today.getMonth() + 1;
+          month = month < 10 ? `0${month}` : month;
+
+          const value = M.Datepicker.getInstance(input).toString();
+          expect(value).toEqual(`${month}/01/${year}`);
+          done();
+        }, 400);
+      }, 400);
+    });
+
+    it('can have a format function', function(done) {
+      const input = document.querySelector('#datepickerInput');
+
+      const today = new Date();
+      const formatFn = (date) => `${date.getFullYear() - 100}-${date.getMonth() + 1}-99`;
+
+      M.Datepicker.init(input, { format: formatFn }).open();
+      M.Datepicker.getInstance(input).open();
+
+      setTimeout(function() {
+        const day1 = document.querySelector('.datepicker-modal button[data-day="1"]');
+        day1.click();
+
+        setTimeout(function() {
+          const year = today.getFullYear() - 100;
+          const month = today.getMonth() + 1;
+
+          const value = M.Datepicker.getInstance(input).toString();
+          expect(value).toEqual(`${year}-${month}-99`);
           done();
         }, 400);
       }, 400);
